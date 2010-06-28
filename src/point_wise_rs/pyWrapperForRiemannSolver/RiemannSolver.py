543d552a555d3090751d12d219d7ae23af3e6cf4
Amal <amal.ghamdi@kaust.edu.sa>
2010-06-28 04:58:54 +0300

    python wrapper for Riemann solver example

---
+++ src/point_wise_rs/pyWrapperForRiemannSolver/RiemannSolver.py
@@ -0,0 +1,79 @@
+from numpy import random, empty,zeros
+#from numpy import *
+from FRS import vec_rp, pw_rp
+from time import clock
+from pylab import plot,  figure
+
+
+
+
+class RiemannSolver:
+  
+
+    def __init__(self, timeSteps, mwaves, mx, meqn, maux, q = None, aux = None):
+       
+        self.timeSteps = timeSteps
+        self.mwaves = mwaves
+        self.mx = mx
+        self.meqn = meqn
+        self.maux = maux
+        if q is not None:
+            self.q = q
+        if aux is not None:
+            self.aux = aux
+
+    def solveVectorized(self, timer = None):
+       
+        if timer is not None:
+            print "Solving... vectorized"
+            start = clock()
+
+        for counter in range(self.timeSteps):
+            waves, s = vec_rp(self.mwaves,self.q[:,0:self.mx-1],self.q[:,1:self.mx],self.aux[:,0:self.mx-1],self.aux[:,1:self.mx])
+
+        if timer is not None:
+            end = clock()
+            self.elapsedTime = end-start
+            print "elapsed time: ", self.elapsedTime, " seconds"
+
+        return waves, s
+
+
+
+
+    def solvePointwize(self, timer = None):
+
+        timeSteps, mwaves, mx, meqn, maux, q , aux = self.timeSteps, self.mwaves, self.mx, self.meqn, self.maux, self.q , self.aux
+        waves = zeros((meqn, mwaves, mx))
+        s = zeros((mwaves, mx))
+        if timer is not None:
+            print "Solving... point-wize"
+            start = clock()
+
+        for counter in range(timeSteps):
+            for i in range(1,mx-1):  #from 1 to mx-1 unlike fortran (from 1 to mx-2 means including mx-2)
+                waves[:,:,i], s[:,i] = pw_rp(mwaves,q[:,i],q[:,i+1],aux[:,i],aux[:,i+1]) # instead of pw_rp(mwaves,q[:,i-1],q[:,i],aux[:,i-1],aux[:,i]) due to the
+                                                                                         # differences in indexing among fortran and python
+
+        if timer is not None:
+            end = clock()
+            self.elapsedTime = end-start
+            print "elapsed time: ", self.elapsedTime, " seconds"
+
+        return waves, s
+        
+
+        
+
+if __name__ == "__main__":
+    rs = RiemannSolver( 1, 2, 2**10, 2, 2)
+    
+    rs.q = random.random((rs.meqn,rs.mx))   
+    rs.aux = random.random((rs.maux,rs.mx))
+
+    
+    waves1, s1  = rs.solveVectorized(timer = True)
+
+    
+    waves2, s2  = rs.solvePointwize(timer = True)
+   
---
+++ src/point_wise_rs/pyWrapperForRiemannSolver/riemannSolverDemo.py
@@ -0,0 +1,20 @@
+
+import RiemannSolver
+from RiemannSolver import *
+from pylab import plot,  figure, suptitle
+
+rs = RiemannSolver(timeSteps =1, mwaves = 2, mx = 800, meqn = 2, maux = 2)
+    
+rs.q = random.random((rs.meqn,rs.mx))
+rs.aux = random.random((rs.maux,rs.mx))
+waves1, s1  = rs.solveVectorized(timer = True)
+waves2, s2  = rs.solvePointwize(timer = True)
+
+
+
+figure(1)
+waveIndex = 0
+componentIndex = 0
+
+suptitle("Figure 1: shows the curve of wave number {0}, for the component number {1}".format(waveIndex+1, componentIndex+2))	
+plot(waves1[componentIndex,waveIndex,:], "g")
