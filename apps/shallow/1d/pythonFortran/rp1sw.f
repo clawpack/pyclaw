d2ad45c87d39fbbef86cb8a9e45b244f59dd3d2f
Matteo Parsani <parsani.matteo@gmail.com>
2011-04-11 14:42:20 +0300

    Shallow water 1D with fortran kernel working

---
+++ apps/shallow/1d/pythonFortran/rp1sw.f
@@ -0,0 +1,194 @@
+c =========================================================
+      subroutine rp1(maxmx,meqn,mwaves,mbc,mx,ql,qr,auxl,auxr,
+     &		       wave,s,amdq,apdq)
+c =========================================================
+c
+c     # solve Riemann problems for the 1D shallow water equations
+c     #   (h)_t + (u h)_x = 0 
+c     #   (uh)_t + ( uuh + .5*gh^2 )_x = 0 
+c     # using Roe's approximate Riemann solver with entropy fix for
+c     # transonic rarefractions.  
+c
+c     # On input, ql contains the state vector at the left edge of each cell
+c     #           qr contains the state vector at the right edge of each cell
+c     # On output, wave contains the waves, 
+c     #            s the speeds, 
+c     #            amdq the  left-going flux difference  A^- \Delta q
+c     #            apdq the right-going flux difference  A^+ \Delta q
+c
+c     # Note that the i'th Riemann problem has left state qr(i-1,:)
+c     #                                    and right state ql(i,:)
+c     # From the basic clawpack routine step1, rp is called with ql = qr = q.
+c
+c     Here meqn=mwaves=2 should be passed from the calling routine
+c
+      implicit double precision (a-h,o-z)
+      dimension   ql(meqn,         1-mbc:maxmx+mbc)
+      dimension   qr(meqn,         1-mbc:maxmx+mbc)
+      dimension    s(mwaves,       1-mbc:maxmx+mbc)
+      dimension wave(meqn, mwaves, 1-mbc:maxmx+mbc)
+      dimension amdq(meqn,         1-mbc:maxmx+mbc)
+      dimension apdq(meqn,         1-mbc:maxmx+mbc)
+c      dimension auxl(*,            1-mbc:maxmx+mbc)
+c      dimension auxr(*,            1-mbc:maxmx+mbc)
+
+      double precision :: grav
+c
+c     # local storage
+c     ---------------
+      dimension delta(2)
+      logical efix
+c      common /comrp/ grav
+      data efix /.true./    !# use entropy fix for transonic rarefactions
+
+      grav = 1.d0
+          
+c
+      do 30 i=2-mbc,mx+mbc
+c
+c         
+c     # compute  Roe-averaged quantities: 
+         ubar = (qr(2,i-1)/dsqrt(qr(1,i-1)) + ql(2,i)/dsqrt(ql(1,i)))/
+     .       ( dsqrt(qr(1,i-1)) + dsqrt(ql(1,i)) )
+         cbar=dsqrt(0.5d0*grav*(qr(1,i-1) + ql(1,i)))
+         
+c     # delta(1)=h(i)-h(i-1) and  delta(2)=hu(i)-hu(i-1)
+      delta(1) = ql(1,i) - qr(1,i-1)
+      delta(2) = ql(2,i) - qr(2,i-1)
+
+c # compute coeffs in the evector expansion of delta(1),delta(2)
+      a1 = 0.5d0*(-delta(2) + (ubar + cbar) * delta(1))/cbar
+      a2 = 0.5d0*( delta(2) - (ubar - cbar) * delta(1))/cbar
+
+c     # finally, compute the waves.
+         wave(1,1,i) = a1
+         wave(2,1,i) = a1*(ubar - cbar)
+         s(1,i) = ubar - cbar
+         
+         wave(1,2,i) = a2
+         wave(2,2,i) = a2*(ubar + cbar)
+         s(2,i) = ubar + cbar
+         
+   30 continue
+
+c     # compute Godunov flux f0:
+c     --------------------------
+
+      if (efix) go to 110
+
+c     # no entropy fix
+c     ----------------------------------------------
+c     # amdq = SUM s*wave   over left-going waves
+c     # apdq = SUM s*wave   over right-going waves
+
+      do 100 m=1,2
+         do 100 i=2-mbc, mx+mbc
+            amdq(m,i) = 0.d0
+            apdq(m,i) = 0.d0
+            do 90 mw=1,mwaves
+               if (s(mw,i) .lt. 0.d0) then
+                   amdq(m,i) = amdq(m,i) + s(mw,i)*wave(m,mw,i)
+                 else
+                   apdq(m,i) = apdq(m,i) + s(mw,i)*wave(m,mw,i)
+                 endif
+   90          continue
+  100       continue
+      go to 900
+c
+c    -----------------------------------------------
+c
+
+  110 continue
+
+c     # With entropy fix
+c     ------------------
+c
+c    # compute flux differences amdq and apdq.
+c    # First compute amdq as sum of s*wave for left going waves.
+c    # Incorporate entropy fix by adding a modified fraction of wave
+c    # if s should change sign.
+c
+      do 200 i=2-mbc,mx+mbc
+      
+c ------------------------------------------------------
+c        # check 1-wave:
+c        ---------------
+c
+c        # u-c in left state (cell i-1)
+         s0 = qr(2,i-1)/qr(1,i-1) - dsqrt(grav*qr(1,i-1))
+         
+c        # check for fully supersonic case:
+	 if (s0.ge.0.d0 .and. s(1,i).gt.0.d0)  then
+c            # everything is right-going
+	     do 60 m=1,2
+		amdq(m,i) = 0.d0
+   60           continue
+	     go to 200 
+	     endif
+c
+c        # u-c to right of 1-wave
+         hr1  = qr(1,i-1) + wave(1,1,i) 
+         uhr1 = qr(2,i-1) + wave(2,1,i)
+         s1 =  uhr1/hr1 - dsqrt(grav*hr1)
+                 
+         if (s0.lt.0.d0 .and. s1.gt.0.d0) then
+c            # transonic rarefaction in the 1-wave
+	     sfract = s0 * (s1-s(1,i)) / (s1-s0)
+	   else if (s(1,i) .lt. 0.d0) then
+c	     # 1-wave is leftgoing
+	     sfract = s(1,i)
+	   else
+c	     # 1-wave is rightgoing
+             sfract = 0.d0   !# this shouldn't happen since s0 < 0
+	   endif
+
+	 do 120 m=1,2
+	    amdq(m,i) = sfract*wave(m,1,i)
+  120       continue
+  
+c -------------------------------------------------------
+c        # check 2-wave:
+c        ---------------
+c        # u+c in right state  (cell i)
+         s3 = ql(2,i)/ql(1,i) + dsqrt(grav*ql(1,i))
+              
+c        # u+c to left of 2-wave
+         hl2  = ql(1,i) - wave(1,2,i) 
+         uhl2 = ql(2,i) - wave(2,2,i)
+         s2 = uhl2/hl2 + dsqrt(grav*hl2)
+                  
+         if (s2 .lt. 0.d0 .and. s3.gt.0.d0) then
+c            # transonic rarefaction in the 2-wave
+	     sfract = s2 * (s3-s(2,i)) / (s3-s2)
+	   else if (s(2,i) .lt. 0.d0) then
+c            # 2-wave is leftgoing
+	     sfract = s(2,i)
+	   else 
+c            # 2-wave is rightgoing
+	     go to 200
+	   endif
+c
+	 do 160 m=1,2
+	    amdq(m,i) = amdq(m,i) + sfract*wave(m,2,i)
+  160       continue
+  200    continue
+c
+c
+c     # compute the rightgoing flux differences:
+c     # df = SUM s*wave   is the total flux difference and apdq = df - amdq
+c
+      do 220 m=1,2
+	 do 220 i = 2-mbc, mx+mbc
+	    df = 0.d0
+	    do 210 mw=1,mwaves
+	       df = df + s(mw,i)*wave(m,mw,i)
+  210          continue
+	    apdq(m,i) = df - amdq(m,i)
+  220       continue
+
+  900 continue
+      return
+      end
+
+
+
---
+++ apps/shallow/1d/pythonFortran/shallow1D.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+1D shallow water equations.
+"""
+
+    
+def shallow1D(iplot=True,petscPlot=False,useController=True,htmlplot=False):
+    #===========================================================================
+    # Import libraries
+    #===========================================================================
+    import numpy as np
+    from petsc4py import PETSc   
+    from petclaw.grid import Grid
+    from petclaw.grid import Dimension
+    from pyclaw.solution import Solution
+    from petclaw.evolve.clawpack import PetClawSolver1D
+    from pyclaw.controller import Controller
+    from petclaw import plot
+
+    #===========================================================================
+    # Initialize grids and then initialize the solution associated to the grid
+    #===========================================================================
+
+    # Grid:
+    xlower = -5.0
+    xupper = 5.0
+    mx = 500
+    x = Dimension('x',xlower,xupper,mx,mthbc_lower=1,mthbc_upper=1)
+    grid = Grid(x)
+
+    grid.meqn = 2
+    grid.t = 0.0
+
+
+    # Initial solution:
+    grid.init_q_petsc_structures() # This must be called before grid.x.center and such can be accessed.
+    xCenter = grid.x.center
+    q = np.zeros([grid.meqn, len(xCenter)], order = 'F')
+
+    radDam = 0.0
+    hl = 3.
+    ul = 0.
+    hr = 1.
+    ur = 0.
+
+    q[0,:] = hl * (grid.p_center[0] <= radDam) + hr * (grid.p_center[0] > radDam)
+    q[1,:] = hl*ul * (grid.p_center[0] <= radDam) + hr*ur * (grid.p_center[0] > radDam)
+    grid.q=q
+
+    init_solution = Solution(grid)
+
+    #===========================================================================
+    # Setup solver and solver parameters
+    #===========================================================================
+    kernelsType = 'F'
+    solver = PetClawSolver1D(kernelsType = kernelsType)
+    solver.mwaves = 2
+    if kernelsType =='P': solver.set_riemann_solver('shallow_roe')
+    solver.mthlim = [4]*solver.mwaves
+
+    #===========================================================================
+    # Setup controller and controller paramters
+    #===========================================================================
+    claw = Controller()
+    claw.keep_copy = True
+    claw.output_format = 'petsc' # The output format MUST be set to petsc!!
+    claw.tfinal = 2.0
+    claw.solutions['n'] = init_solution
+    claw.solver = solver
+
+
+    #===========================================================================
+    # Solve the problem
+    #===========================================================================
+    status = claw.run()
+
+    if htmlplot: plot.plotHTML()
+    if petscPlot: plot.plotPetsc(output_object)
+    if iplot: plot.plotInteractive()
+
+
+    # Plot
+    if claw.keep_copy:
+        import matplotlib.pyplot as plt
+        for n in xrange(0,claw.nout+1):
+            sol = claw.frames[n]
+            plt.figure(1)
+            plt.subplot(3,2,n+1)
+            plt.plot(x.center,sol.q[0,:])
+            plt.axis([x.lower,x.upper,0.0,3.2])
+            plt.title('Height t = %s' % sol.t)
+            plt.figure(2)
+            plt.subplot(3,2,n+1)
+            plt.plot(x.center,sol.q[1,:])
+            plt.axis([x.lower,x.upper,0.0,4.6])
+            plt.title('Momentum t = %s' % sol.t)
+        plt.show()
+
+if __name__=="__main__":
+    import sys
+    from petclaw.util import _info_from_argv
+    args, kwargs = _info_from_argv(sys.argv)
+    error=shallow1D(*args,**kwargs)
+    print 'Error: ',error
+    
+
+   
+
