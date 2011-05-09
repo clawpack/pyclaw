295364875e52d15158295b46653be21f70107d27
David Ketcheson <dketch@gmail.com>
2011-05-09 22:08:11 +0300

    Moved BCs up to pyclaw.evolve.solver.

--- petclaw/evolve/clawpack.py
+++ petclaw/evolve/clawpack.py
@@ -59,111 +59,20 @@ class PetSolver(Solver):
     """
     
     # ========== Boundary Conditions ==================================
-    def append_ghost_cells(self,state,q):
+    def append_ghost_cells(self,grid,state,q):
+        """
+        Returns q with ghost cells attached.  For PetSolver,
+        this means returning the local vector.  
+        The arguments 'q' and 'grid' are
+        not used here; they are passed only in order to have a common
+        interface for the petclaw and pyclaw versions of this function.
+        """
         state.q_da.globalToLocal(state.gqVec, state.lqVec)
         q_dim = [state.local_n[i] + 2*self.mbc for i in xrange(state.ndim)]
         q_dim.insert(0,state.meqn)
         ghosted_q=state.lqVec.getArray().reshape(q_dim, order = 'F')
         return ghosted_q
  
-    def qbc(self,grid,state):
-        """
-        Returns an array with the ghost cells filled.
-        """
-        
-        #This line causes us to use grid.mbc.  We should get rid of it.
-        #ghost_q should be a method of solver.
-        qbc = self.append_ghost_cells(state,state.q)
-
-        for i,dim in enumerate(grid.dimensions):
-            #If a user defined boundary condition is being used, send it on,
-            #otherwise roll the axis to front position and operate on it
-            if dim.mthbc_lower == 0:
-                self.qbc_lower(grid,dim,qbc)
-            else:
-                self.qbc_lower(grid,dim,np.rollaxis(qbc,i+1,1))
-            if dim.mthbc_upper == 0:
-                self.qbc_upper(grid,dim,qbc)
-            else:
-                self.qbc_upper(grid,dim,np.rollaxis(qbc,i+1,1))
-        return qbc
-
-    def qbc_lower(self,grid,dim,qbc):
-        r"""
-        This function should be upstreamed to the pyclaw.evolve.solver.Solver class
-        """
-        # User defined functions
-        if dim.mthbc_lower == 0: self.user_bc_lower(grid,dim,qbc)
-        # Zero-order extrapolation
-        elif dim.mthbc_lower == 1:
-            if dim.nstart == 0:
-                for i in xrange(self.mbc):
-                    qbc[:,i,...] = qbc[:,self.mbc,...]
-        # Periodic
-        elif dim.mthbc_lower == 2:
-            pass # Amal: this is implemented automatically by petsc4py
-            
-        # Solid wall bc
-        elif dim.mthbc_lower == 3:
-             if dim.nstart == 0:
-                if grid.ndim == 1:
-                    for i in xrange(self.mbc):
-                        qbc[:,i,...] = qbc[:,self.mbc+1-i,...]
-                        qbc[1,i,...] = -qbc[1,self.mbc+1-i,...] # Negate normal velocity
-                elif grid.ndim == 2:
-                     if dim.name == 'x':  # left boundary in the x direction
-                         for i in xrange(self.mbc):
-                             qbc[:,i,...] = qbc[:,self.mbc+1-i,...]
-                             qbc[1,i,...] = -qbc[1,self.mbc+1-i,...] # Negate normal velocity
-                     else: # lower boundary in the y direction
-                         for i in xrange(self.mbc):
-                             qbc[:,i,...] = qbc[:,self.mbc+1-i,...]
-                             qbc[2,i,...] = -qbc[2,self.mbc+1-i,...]  # Negate normal velocity
-              
-                else:
-                    raise NotImplementedError("3D wall boundary condition %s not implemented" % x.mthbc_lower)
-        else:
-            raise NotImplementedError("Boundary condition %s not implemented" % x.mthbc_lower)
-
-
-    def qbc_upper(self,grid,dim,qbc):
-        r"""
-        This function should be upstreamed to the pyclaw.evolve.solver.Solver class
-        """
-        # User defined functions
-        if dim.mthbc_upper == 0: self.user_bc_upper(grid,dim,qbc)
-        # Zero-order extrapolation
-        elif dim.mthbc_upper == 1:
-            if dim.nend == dim.n :
-                for i in xrange(self.mbc):
-                    qbc[:,-i-1,...] = qbc[:,-self.mbc-1,...] 
- 	    
-        elif dim.mthbc_upper == 2:
-            # Periodic
-            pass # Amal: this is implemented automatically by petsc4py
-
-        # Solid wall bc
-        elif dim.mthbc_upper == 3:
-            if dim.nend == dim.n:
-                if grid.ndim == 1:
-                    for i in xrange(self.mbc):
-                        qbc[:,-i-1,...] = qbc[:,-self.mbc-2+i,...]
-                        qbc[1,-i-1,...] = -qbc[1,-self.mbc-2+i,...] # Negate normal velocity
-                elif grid.ndim == 2:
-                     if dim.name == 'x': # right boundary in the x direction
-                         for i in xrange(self.mbc):
-                             qbc[:,-i-1,...] = qbc[:,-self.mbc-2+i,...]
-                             qbc[1,-i-1,...] = -qbc[1,-self.mbc-2+i,...] # Negate normal velocity
-                     else: # upper boundary in the y direction
-                         for i in xrange(self.mbc):
-                             qbc[:,-i-1,...] = qbc[:,-self.mbc-2+i,...]
-                             qbc[2,-i-1,...] = -qbc[2,-self.mbc-2+i,...] # Negate normal velocity
-              
-                else:
-                    raise NotImplementedError("3D wall boundary condition %s not implemented" % x.mthbc_lower)
-        else:
-            raise NotImplementedError("Boundary condition %s not implemented" % x.mthbc_lower)
-
     def communicateCFL(self):
         if self.dt_variable:
           comm = MPI.COMM_WORLD #Amal:should be consistent with petsc commworld
