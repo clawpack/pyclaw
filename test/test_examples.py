7ff3e2f7de0de0261222345d38a93db64b049728
Matteo Parsani <parsani.matteo@gmail.com>
2011-05-22 15:03:51 +0300

    Refactor 1D regression tests in test_examples.py with attributes. Now nose can test subclasses of regression tests. Fiexd petsc implicit TS with sharpclaw

--- test/test_examples.py
+++ test/test_examples.py
@@ -1,3 +1,19 @@
+###############################################################################################
+# This python script contains regression and unit tests for PYCLAW and PETCLAW
+# These test are executed with nose (http://somethingaboutorange.com/mrl/projects/nose/1.0.0/)
+# Attributes are use to tag each regression and unit test. This way nose can run different type 
+# of subclasses regression tests.
+#
+#
+# Use: nosetests -a attribute-1 = string-1,attribute-2 = string-2,attribute-3 = string-3
+#      for logic AND
+#
+# Use: nosetests -a attribute-1 = string-1 -a attribute-2 = string-2 -a attribute-3 = string-3
+#      for logic OR
+#
+################################################################################################
+
+
 from nose.plugins.attrib import attr
 import util                      
 
@@ -5,82 +21,153 @@ if __name__=="__main__":
     import nose
     nose.main()
 
-@attr(examples='implicit')
-def test_implicit():
+
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='sharpclaw')
+@attr(kernel_language='fortran')
+@attr(petsc=True)
+@attr(ts='implicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1a():
     path           = './test/acoustics/1d/homogeneous'
     target_name    = 'all'
     module_name    = 'acoustics_implicit'
-    method_name    = 'acoustics'
-    method_options = {'sclaw': 1, 'petscts': 1}
+    problem_name    = 'acoustics'
+    method_options = {'sclaw': 1, 'petscts': 1, '-ts_type': theta}
     verifier       = lambda error: abs(error-0.00220809553637)<1.e-5
-    yield(util.build_run_verify, path, target_name, module_name, method_name, verifier, method_options)
+    yield(util.build_run_verify, path, target_name, module_name, problem_name, verifier, method_options)
 
-def test_examples():
-    path        = './test/acoustics/1d/homogeneous'
-    name        = 'acoustics'
-    target_name = 'step1.so'
+
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='classic')
+@attr(kernel_language='python')
+@attr(petsc=False)
+@attr(ts='explicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1b():
+    path           = './test/acoustics/1d/homogeneous'
+    module_name    = 'acoustics'
+    problem_name   = 'acoustics'
+    target_name    = 'step1.so'    
     method_options = {'kernel_language' : 'Fortran', 'use_PETSc' : False, 'soltype' : 'classic'}
-    verifier    = lambda error: abs(error-0.00104856594174)<1.e-5
+    verifier       = lambda error: abs(error-0.00104856594174)<1.e-5
     yield(util.build_run_verify, path, target_name, name, name, verifier, method_options)
-
-    method_options = {'kernel_language' : 'Python', 'use_PETSc' : False, 'soltype' : 'classic'}
-    yield(util.run_verify, path, name, name, verifier, method_options)
-
-    method_options = {'kernel_language' : 'Fortran', 'use_PETSc' : True, 'soltype' : 'classic'}
-    yield(util.build_run_verify, path, target_name, name, name, verifier, method_options)
-
-    method_options = {'kernel_language' : 'Python', 'use_PETSc' : True, 'soltype' : 'classic'}
-    yield(util.run_verify, path, name, name, verifier, method_options)
-
-    verifier    = lambda error: abs(error-0.000818286913339)<1.e-5
-    target_name = 'flux1.so'
-    method_options = {'kernel_language' : 'Python', 'use_PETSc' : False, 'soltype' : 'sharpclaw'}
-    yield(util.run_verify, path, name, name, verifier, method_options)
-
-    method_options = {'kernel_language' : 'Fortran', 'use_PETSc' : False, 'soltype' : 'sharpclaw'}
-    yield(util.build_run_verify, path, target_name, name, name, verifier, method_options)
-
-    method_options = {'kernel_language' : 'Python', 'use_PETSc' : True, 'soltype' : 'sharpclaw'}
-    yield(util.run_verify, path, name, name, verifier, method_options)
-
-    method_options = {'kernel_language' : 'Fortran', 'use_PETSc' : True, 'soltype' : 'sharpclaw'}
-    yield(util.build_run_verify, path, target_name, name, name, verifier, method_options)
-
     
-#=======================================================
-#    2D tests
-#=======================================================
-    path        = './test/acoustics/2d/homogeneous'
-    module_name = 'acoustics'
-    target_name = 'dimsp2.so'
-    method_name = 'acoustics2D'
 
-    def verify_acoustics2D(test_x):
-        import numpy
-        verify_x=numpy.loadtxt('test/acoustics2D_solution')
-        return (numpy.linalg.norm(test_x-verify_x)<1.e-14)
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='classic')
+@attr(kernel_language='python')
+@attr(petsc=False)
+@attr(ts='explicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1c():
+    path           = './test/acoustics/1d/homogeneous'
+    module_name    = 'acoustics'
+    problem_name   = 'acoustics'
+    method_options = {'kernel_language' : 'Python', 'use_PETSc' : False, 'soltype' : 'classic'}
+    verifier       = lambda error: abs(error-0.00104856594174)<1.e-5
+    yield(util.run_verify, path, module_name, problem_name, verifier, method_options)
+    
 
-    method_options = {'use_PETSc' : True}
-    yield(util.build_run_verify, path, target_name, module_name, method_name, verify_acoustics2D, {})
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='classic')
+@attr(kernel_language='fortran')
+@attr(petsc=True)
+@attr(ts='explicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1d():
+    path           = './test/acoustics/1d/homogeneous'
+    target_name    = 'all'
+    module_name    = 'acoustics'
+    problem_name    = 'acoustics'
+    method_options = {'kernel_language' : 'Fortran', 'use_PETSc' : True, 'soltype' : 'classic'}
+    verifier       = lambda error: abs(error-0.00104856594174)<1.e-5
+    yield(util.build_run_verify, path, target_name, module_name, problem_name, verifier, method_options)
+    
 
-    method_options = {'use_PETSc' : False}
-    yield(util.build_run_verify, path, target_name, module_name, method_name, verify_acoustics2D, method_options)    
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='classic')
+@attr(kernel_language='python')
+@attr(petsc=True)
+@attr(ts='explicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1e():
+    path           = './test/acoustics/1d/homogeneous'
+    module_name    = 'acoustics'
+    problem_name   = 'acoustics'
+    method_options = {'kernel_language' : 'Python', 'use_PETSc' : True, 'soltype' : 'classic'}
+    verifier       = lambda error: abs(error-0.00104856594174)<1.e-5
+    yield(util.run_verify, path, module_name, problem_name, verifier, method_options)
 
-#=======================================================
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='sharpclaw')
+@attr(kernel_language='python')
+@attr(petsc=False)
+@attr(ts='explicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1f():  
+    path           = './test/acoustics/1d/homogeneous'
+    module_name    = 'acoustics'
+    problem_name   = 'acoustics'
+    method_options = {'kernel_language' : 'Python', 'use_PETSc' : False, 'soltype' : 'sharpclaw'}
+    verifier       = lambda error: abs(error-0.000818286913339)<1.e-5
+    yield(util.run_verify, path, module_name, problem_name, verifier, method_options)
 
-    path        = './test/euler/2d'
-    module_name = 'shockbubble'
-    target_name = 'dimsp2.so'
-    method_name = 'shockbubble'
 
-    def verify_shockbubble(test_x):
-        import numpy
-        verify_x=numpy.loadtxt('test/sb_density')
-        print numpy.max(abs(test_x-verify_x))
-        return numpy.max(abs(test_x-verify_x))<1.e-14
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='sharpclaw')
+@attr(kernel_language='fortran')
+@attr(petsc=False)
+@attr(ts='explicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1g():  
+    path           = './test/acoustics/1d/homogeneous'
+    target_name    = 'flux1.so'
+    module_name    = 'acoustics'
+    problem_name   = 'acoustics'
+    method_options = {'kernel_language' : 'Fortran', 'use_PETSc' : False, 'soltype' : 'sharpclaw'}
+    verifier       = lambda error: abs(error-0.000818286913339)<1.e-5
+    yield(util.build_run_verify, path, target_name, module_name, problem_name, verifier, method_options)
+    
 
-    method_options = {'use_PETSc' : False}
-    #yield(util.build_run_verify, path, target_name, module_name, method_name, verify_shockbubble, method_options)
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='sharpclaw')
+@attr(kernel_language='python')
+@attr(petsc=True)
+@attr(ts='explicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1h():  
+    path           = './test/acoustics/1d/homogeneous'
+    module_name    = 'acoustics'
+    problem_name   = 'acoustics'
+    method_options = {'kernel_language' : 'Python', 'use_PETSc' : True, 'soltype' : 'sharpclaw'}
+    verifier       = lambda error: abs(error-0.000818286913339)<1.e-5
+    yield(util.run_verify, path, module_name, problem_name, verifier, method_options)
 
-    method_options = {'use_PETSc' : True}
-    #yield(util.build_run_verify, path, target_name, module_name, method_name, verify_shockbubble, method_options)
+
+# Regression test: 1D acoustic in homegeneous material
+#@attr(testType ='regression')
+@attr(sd='sharpclaw')
+@attr(kernel_language='fortran')
+@attr(petsc=True)
+@attr(ts='explicit')
+@attr(speed='fast')
+def test_1D_acoustic_homogeneous_1i(): 
+    path           = './test/acoustics/1d/homogeneous'
+    target_name    = 'flux1.so'
+    module_name    = 'acoustics'
+    problem_name   = 'acoustics'
+    method_options = {'kernel_language' : 'Fortran', 'use_PETSc' : True, 'soltype' : 'sharpclaw'}
+    verifier       = lambda error: abs(error-0.000818286913339)<1.e-5
+    yield(util.build_run_verify, path, target_name, module_name, module_name, verifier, method_options)    
+
+
+
