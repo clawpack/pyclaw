29a0a6afdd9fe3d39a4e79d7b37dcda861fd45d7
David Ketcheson <dketch@gmail.com>
2011-06-09 23:38:54 +0300

    Make work array arguments to fortran routines optional by using f2py directive "optional".

--- src/fortran/2d/classic/step2.f
+++ src/fortran/2d/classic/step2.f
@@ -18,23 +18,25 @@ c
 c
       implicit double precision (a-h,o-z)
       external rpn2,rpt2
-      dimension qold(meqn,1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc)
-      dimension qnew(meqn,1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc)
+      double precision qold(meqn,1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc)
+      double precision qnew(meqn,1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc)
+      double precision  q1d(meqn,1-mbc:maxm+mbc)
+      double precision qadd(meqn,1-mbc:maxm+mbc)
+      double precision fadd(meqn,1-mbc:maxm+mbc)
+      double precision gadd(meqn, 2, 1-mbc:maxm+mbc)
+      double precision aux(maux, 1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc)
+      double precision aux1(maux, 1-mbc:maxm+mbc)
+      double precision aux2(maux, 1-mbc:maxm+mbc)
+      double precision aux3(maux, 1-mbc:maxm+mbc)
+
+      double precision dtdx1d(1-mbc:maxm+mbc)
+      double precision dtdy1d(1-mbc:maxm+mbc)
+      integer method(7),mthlim(mwaves)
+      double precision work(mwork)
+
 cf2py intent(in,out) cfl
 cf2py intent(in,out) qnew
-      dimension  q1d(meqn,1-mbc:maxm+mbc)
-      dimension qadd(meqn,1-mbc:maxm+mbc)
-      dimension fadd(meqn,1-mbc:maxm+mbc)
-      dimension gadd(meqn, 2, 1-mbc:maxm+mbc)
-      dimension aux(maux, 1-mbc:maxmx+mbc, 1-mbc:maxmy+mbc)
-      dimension aux1(maux, 1-mbc:maxm+mbc)
-      dimension aux2(maux, 1-mbc:maxm+mbc)
-      dimension aux3(maux, 1-mbc:maxm+mbc)
-
-      dimension dtdx1d(1-mbc:maxm+mbc)
-      dimension dtdy1d(1-mbc:maxm+mbc)
-      dimension method(7),mthlim(mwaves)
-      dimension work(mwork)
+cf2py optional q1d, qadd, fadd, gadd, dtdx1d, dtdy1d
 
       common /comxyt/ dtcom,dxcom,dycom,tcom,icom,jcom
 c
