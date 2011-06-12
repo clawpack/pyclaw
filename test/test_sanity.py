d00afaa76a74b7f7de9bb0eb63c76b471b160eb0
Amal Alghamdi <amal.ghamdi@kaust.edu.sa>
2011-06-12 09:15:54 +0300

    add parallel environment test sanity

--- test/test_sanity.py
+++ test/test_sanity.py
@@ -4,3 +4,21 @@ from nose.plugins.attrib import attr
 def test_sane():
     pass
 
+def test_parallel_env_sane():
+    """
+    Test whether PETSc/MPI are interacting with each other correctly
+    """
+    import subprocess
+    
+    # run subprocess with the required number of processes
+    np = 4
+    file_name = "./test/parallel_env_sanity.py" 
+    run_command = "mpiexec", "-n", str(np) ,"python",file_name, "np="+str(np)
+    
+    p = subprocess.Popen(run_command, stdout=subprocess.PIPE ,stderr=subprocess.STDOUT)
+    (stdout_data, ignore) = p.communicate()
+
+    if int(stdout_data)!=np:
+        raise Exception('Unexpected size of PETSc.COMM_WORLD, size = '+stdout_data.strip()+' while number of processes = '+str(np)+'. Check your PETSc/MPI installation')
+    else:
+        pass
