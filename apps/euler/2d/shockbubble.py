27a9ffd783640d3fa5c9b8942c26fc0634020745
Amal Alghamdi <amal.ghamdi@kaust.edu.sa>
2011-06-16 13:35:36 +0300

    wrapping src2 fortran routine

--- apps/euler/2d/shockbubble.py
+++ apps/euler/2d/shockbubble.py
@@ -19,12 +19,13 @@ def qinit(state,x0=0.5,y0=0.,r0=0.2,rhoin=0.1,pinf=5.):
     y =state.grid.y.center
     Y,X = np.meshgrid(y,x)
     r = np.sqrt((X-x0)**2 + (Y-y0)**2)
-
+    
     state.q[0,:,:] = rhoin*(r<=r0) + rhoout*(r>r0)
     state.q[1,:,:] = 0.
     state.q[2,:,:] = 0.
     state.q[3,:,:] = (pin*(r<=r0) + pout*(r>r0))/gamma1
     state.q[4,:,:] = 1.*(r<=r0)
+
 
 def auxinit(state):
     """
@@ -60,38 +61,51 @@ def euler_rad_src(solver,solutions,t,dt):
     Geometric source terms for Euler equations with radial symmetry.
     Integrated using a 2-stage, 2nd-order Runge-Kutta method.
     """
+    srcterm_language = 'Python'
+    state = solutions['n'].states[0]
+    aux=state.aux
+    grid = state.grid
+    q = state.q
     
-    dt2 = dt/2.
-    press = 0.
-    ndim = 2
+    if  srcterm_language == 'Python':
+        dt2 = dt/2.
+        press = 0.
+        ndim = 2
 
-    aux=solutions['n'].states[0].aux
-    q = solutions['n'].states[0].q
 
-    rad = aux[0,:,:]
+        rad = aux[0,:,:]
 
-    rho = q[0,:,:]
-    u   = q[1,:,:]/rho
-    v   = q[2,:,:]/rho
-    press  = gamma1 * (q[3,:,:] - 0.5*rho*(u**2 + v**2))
+        rho = q[0,:,:]
+        u   = q[1,:,:]/rho
+        v   = q[2,:,:]/rho
+        press  = gamma1 * (q[3,:,:] - 0.5*rho*(u**2 + v**2))
 
-    qstar = np.empty(q.shape)
+        qstar = np.empty(q.shape)
 
-    qstar[0,:,:] = q[0,:,:] - dt2*(ndim-1)/rad * q[2,:,:]
-    qstar[1,:,:] = q[1,:,:] - dt2*(ndim-1)/rad * rho*u*v
-    qstar[2,:,:] = q[2,:,:] - dt2*(ndim-1)/rad * rho*v*v
-    qstar[3,:,:] = q[3,:,:] - dt2*(ndim-1)/rad * v * (q[3,:,:] + press)
+        qstar[0,:,:] = q[0,:,:] - dt2*(ndim-1)/rad * q[2,:,:]
+        qstar[1,:,:] = q[1,:,:] - dt2*(ndim-1)/rad * rho*u*v
+        qstar[2,:,:] = q[2,:,:] - dt2*(ndim-1)/rad * rho*v*v
+        qstar[3,:,:] = q[3,:,:] - dt2*(ndim-1)/rad * v * (q[3,:,:] + press)
 
-    rho = qstar[0,:,:]
-    u   = qstar[1,:,:]/rho
-    v   = qstar[2,:,:]/rho
-    press  = gamma1 * (qstar[3,:,:] - 0.5*rho*(u**2 + v**2))
+        rho = qstar[0,:,:]
+        u   = qstar[1,:,:]/rho
+        v   = qstar[2,:,:]/rho
+        press  = gamma1 * (qstar[3,:,:] - 0.5*rho*(u**2 + v**2))
 
-    q[0,:,:] = q[0,:,:] - dt2*(ndim-1)/rad * qstar[2,:,:]
-    q[1,:,:] = q[1,:,:] - dt2*(ndim-1)/rad * rho*u*v
-    q[2,:,:] = q[2,:,:] - dt2*(ndim-1)/rad * rho*v*v
-    q[3,:,:] = q[3,:,:] - dt2*(ndim-1)/rad * v * (qstar[3,:,:] + press)
-
+        q[0,:,:] = q[0,:,:] - dt2*(ndim-1)/rad * qstar[2,:,:]
+        q[1,:,:] = q[1,:,:] - dt2*(ndim-1)/rad * rho*u*v
+        q[2,:,:] = q[2,:,:] - dt2*(ndim-1)/rad * rho*v*v
+        q[3,:,:] = q[3,:,:] - dt2*(ndim-1)/rad * v * (qstar[3,:,:] + press)
+    else:
+        from classic2 import src2
+        qbc = solver.append_ghost_cells(state)
+        auxbc = solver.append_ghost_cells_to_aux(state)
+        maxmx, maxmy, mx, my = grid.ng[0], grid.ng[1], grid.ng[0], grid.ng[1]
+        mbc = solver.mbc
+        xlower, ylower, dx, dy =0,0,0,0 # it is not used
+        #t, dt = passed
+        qbc = src2(maxmx,maxmy,mbc,mx,my,xlower,ylower,dx,dy,qbc,auxbc,t,dt)
+        solver.set_global_q(state, qbc)
 
 def shockbubble(use_petsc=False,iplot=False,htmlplot=False,outdir='./_output',solver_type='classic'):
     """
@@ -122,7 +136,7 @@ def shockbubble(use_petsc=False,iplot=False,htmlplot=False,outdir='./_output',so
     solver.mthauxbc_upper[1]=pyclaw.BC.outflow
 
     # Initialize grid
-    mx=160; my=40
+    mx=600; my=200
     x = pyclaw.Dimension('x',0.0,2.0,mx)
     y = pyclaw.Dimension('y',0.0,0.5,my)
     grid = pyclaw.Grid([x,y])
@@ -143,7 +157,7 @@ def shockbubble(use_petsc=False,iplot=False,htmlplot=False,outdir='./_output',so
 
     claw = pyclaw.Controller()
     claw.keep_copy = True
-    claw.tfinal = 0.25
+    claw.tfinal = 0.4
     claw.solution = pyclaw.Solution(state)
     claw.solver = solver
     claw.nout = 10
