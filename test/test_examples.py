4d00e89e874c49365696b1fad44ea1e957d0b2a3
Matteo Parsani <parsani.matteo@gmail.com>
2011-10-17 10:10:19 +0300

    Correct SW sphere regression test attributes.

--- test/test_examples.py
+++ test/test_examples.py
@@ -451,8 +451,7 @@ def test_psystem_petclaw_classic():
 @attr(kernel_language='fortran')
 @attr(petsc=False)
 @attr(time_stepping_mode='explicit')
-@attr(speed='very slow')
-@attr(which='this')
+@attr(speed='slow')
 def test_2D_shallowwatersphere(): 
     path           = './test/shallow_sphere'
     target_name    = 'classic2.so'
