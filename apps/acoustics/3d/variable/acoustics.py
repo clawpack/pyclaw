f5d88e311ada8b46e1d634d6889f67ae05e804fc
David Ketcheson <dketch@gmail.com>
2011-11-14 11:18:37 +0300

    Acoustics test is converging in serial and parallel.

--- apps/acoustics/3d/variable/acoustics.py
+++ apps/acoustics/3d/variable/acoustics.py
@@ -19,27 +19,27 @@ def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',so
         raise Exception('Unrecognized solver_type.')
 
     solver.dim_split=False
-    solver.mwaves = 3
+    solver.mwaves = 2
     solver.limiters = pyclaw.limiters.tvd.MC
 
-    solver.bc_lower[0]=pyclaw.BC.reflecting
-    solver.bc_upper[0]=pyclaw.BC.outflow
-    solver.bc_lower[1]=pyclaw.BC.reflecting
-    solver.bc_upper[1]=pyclaw.BC.outflow
-    solver.bc_lower[2]=pyclaw.BC.reflecting
-    solver.bc_upper[2]=pyclaw.BC.outflow
+    solver.bc_lower[0]=pyclaw.BC.periodic
+    solver.bc_upper[0]=pyclaw.BC.periodic
+    solver.bc_lower[1]=pyclaw.BC.periodic
+    solver.bc_upper[1]=pyclaw.BC.periodic
+    solver.bc_lower[2]=pyclaw.BC.periodic
+    solver.bc_upper[2]=pyclaw.BC.periodic
 
-    solver.aux_bc_lower[0]=pyclaw.BC.reflecting
-    solver.aux_bc_upper[0]=pyclaw.BC.outflow
-    solver.aux_bc_lower[1]=pyclaw.BC.reflecting
-    solver.aux_bc_upper[1]=pyclaw.BC.outflow
-    solver.aux_bc_lower[2]=pyclaw.BC.reflecting
-    solver.aux_bc_upper[2]=pyclaw.BC.outflow
+    solver.aux_bc_lower[0]=pyclaw.BC.periodic
+    solver.aux_bc_upper[0]=pyclaw.BC.periodic
+    solver.aux_bc_lower[1]=pyclaw.BC.periodic
+    solver.aux_bc_upper[1]=pyclaw.BC.periodic
+    solver.aux_bc_lower[2]=pyclaw.BC.periodic
+    solver.aux_bc_upper[2]=pyclaw.BC.periodic
 
     solver.dim_split=True
 
     # Initialize grid
-    mx=30; my=30; mz=30
+    mx=128; my=8; mz=16
     x = pyclaw.Dimension('x',-1.0,1.0,mx)
     y = pyclaw.Dimension('y',-1.0,1.0,my)
     z = pyclaw.Dimension('z',-1.0,1.0,mz)
@@ -49,8 +49,8 @@ def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',so
     maux = 2 # density, sound speed
     state = pyclaw.State(grid,meqn,maux)
 
-    zl = 2.0  # Impedance in left half
-    cl = 2.0  # Sound speed in left half
+    zl = 1.0  # Impedance in left half
+    cl = 1.0  # Sound speed in left half
     zr = 1.0  # Impedance in right half
     cr = 1.0  # Sound speed in right half
 
@@ -61,24 +61,39 @@ def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',so
     state.aux[1,:,:,:] = cl*(X<0.) + cr*(X>=0.) # Sound speed
 
     x0 = -0.5; y0 = 0.; z0 = 0.
-    r = np.sqrt((X-x0)**2 + (Y-y0)**2 + (Z-z0)**2)
+    r = np.sqrt((X-x0)**2) #+ (Y-y0)**2 + (Z-z0)**2)
     width=0.2
-    state.q[0,:,:,:] = (np.abs(r-0.5)<=width)*(1.+np.cos(np.pi*(r-0.5)/width))
+    state.q[0,:,:,:] = (np.abs(r)<=width)*(1.+np.cos(np.pi*(r)/width))
     state.q[1,:,:,:] = 0.
     state.q[2,:,:,:] = 0.
+    state.q[3,:,:,:] = 0.
 
     claw = pyclaw.Controller()
-    claw.keep_copy = False
+    claw.keep_copy = True
     claw.solution = pyclaw.Solution(state)
     claw.solver = solver
     claw.outdir=outdir
 
     # Solve
-    claw.tfinal = 0.6
+    claw.tfinal = 2.0
     status = claw.run()
 
     if htmlplot:  pyclaw.plot.html_plot(outdir=outdir,format=claw.output_format)
     if iplot:     pyclaw.plot.interactive_plot(outdir=outdir,format=claw.output_format)
+
+    if use_petsc:
+        pinitial=claw.frames[0].state.gqVec.getArray().reshape([state.meqn,grid.ng[0],grid.ng[1],grid.ng[2]])[0,:,:,:].reshape(-1)
+        pmiddle=claw.frames[claw.nout/2].state.gqVec.getArray().reshape([state.meqn,grid.ng[0],grid.ng[1],grid.ng[2]])[0,:,:,:].reshape(-1)
+        pfinal=claw.frames[claw.nout].state.gqVec.getArray().reshape([state.meqn,grid.ng[0],grid.ng[1],grid.ng[2]])[0,:,:,:].reshape(-1)
+    else:
+        pinitial=claw.frames[0].state.q[0,:,:,:].reshape(-1)
+        pmiddle  =claw.frames[3].state.q[0,:,:,:].reshape(-1)
+        pfinal  =claw.frames[claw.nout].state.q[0,:,:,:].reshape(-1)
+
+    print grid.d
+    print 'Final error: ', np.prod(grid.d)*np.linalg.norm(pfinal-pinitial,ord=1)
+    #print 'Middle error: ', np.prod(grid.d)*np.linalg.norm(pmiddle-pinitial,ord=1)
+
 
 if __name__=="__main__":
     import sys
