8300eda052f56fb3a49a3c5a103bb4aff949b10a
David Ketcheson <dketch@gmail.com>
2011-11-27 11:22:19 +0300

    Remove unneeded import statements.

--- src/petclaw/cfl.py
+++ src/petclaw/cfl.py
@@ -16,7 +16,6 @@ class CFL(object):
         This is used to determine whether the CFL condition was
         violated and adjust the timestep.
         """
-        from petsc4py import PETSc
         self._reduce_vec.array = self._local_max
         self._global_max = self._reduce_vec.max()[1]
         return self._global_max
@@ -28,7 +27,6 @@ class CFL(object):
         self._local_max = new_local_max
 
     def update_global_max(self,new_local_max):
-        from petsc4py import PETSc
         self._reduce_vec.array = new_local_max
         self._global_max = self._reduce_vec.max()[1]
 
