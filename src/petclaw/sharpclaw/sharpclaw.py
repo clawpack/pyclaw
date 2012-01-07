fed7095e0c36ea94c7b2e6a5db80d85be80acb95
David Ketcheson <dketch@gmail.com>
2012-01-07 21:09:54 +0300

    Move python solver code to subdirectories.

--- src/petclaw/sharpclaw/sharpclaw.py
+++ src/petclaw/sharpclaw/sharpclaw.py
@@ -3,15 +3,15 @@
 r"""
 Module containing SharpClaw solvers for PetClaw
 """
-import pyclaw.sharpclaw
+import pyclaw.sharpclaw.sharpclaw
 
 
-class SharpClawSolver1D(pyclaw.sharpclaw.SharpClawSolver1D):
+class SharpClawSolver1D(pyclaw.sharpclaw.sharpclaw.SharpClawSolver1D):
     """
     1D parallel SharpClaw solver.  See the corresponding PyClaw class for documentation.
     """
 
-class SharpClawSolver2D(pyclaw.sharpclaw.SharpClawSolver2D):
+class SharpClawSolver2D(pyclaw.sharpclaw.sharpclaw.SharpClawSolver2D):
     """
     2D parallel SharpClaw solver.  See the corresponding PyClaw class for documentation.
     """
