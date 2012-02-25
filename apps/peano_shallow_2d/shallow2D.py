a8d6bcfd067a87dbbfdf0c4a034bdbd7315906bb
Kristof Unterweger <unterweg@in.tum.de>
2012-02-25 11:15:05 +0300

    saved changes to local branch

---
+++ apps/peano_shallow_2d/shallow2D.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+2D shallow water equations.
+"""
+#===========================================================================
+# Import libraries
+#===========================================================================
+
+import numpy as np
+from petclaw import plot
+#import pdb  # Debugger
+
+
+def qinit(state,hl,ul,vl,hr,ur,vr,radDam):
+    x0=0.
+    y0=0.
+    xCenter = state.grid.x.centers
+    yCenter = state.grid.y.centers
+    Y,X = np.meshgrid(yCenter,xCenter)
+    r = np.sqrt((X-x0)**2 + (Y-y0)**2)
+    state.q[0,:,:] = hl*(r<=radDam) + hr*(r>radDam)
+    state.q[1,:,:] = hl*ul*(r<=radDam) + hr*ur*(r>radDam)
+    state.q[2,:,:] = hl*vl*(r<=radDam) + hr*vr*(r>radDam)
+
+    
+def shallow2D(use_petsc=False,iplot=0,htmlplot=False,outdir='./_output',solver_type='classic'):
+    #===========================================================================
+    # Import libraries
+    #===========================================================================
+    import numpy as np
+    from pyclaw.peano import PeanoSolver
+
+    if use_petsc:
+        import petclaw as pyclaw
+    else:
+        import pyclaw
+
+    #===========================================================================
+    # Setup solver and solver parameters
+    #===========================================================================
+    subdivisionFactor = 4.
+    if solver_type == 'classic':
+        solver = pyclaw.ClawSolver2D()
+    elif solver_type == 'sharpclaw':
+        solver = pyclaw.SharpClawSolver2D()
+    peanoSolver = PeanoSolver(solver, (1./3.)/subdivisionFactor)
+
+    solver.num_waves = 3
+    solver.limiters = pyclaw.limiters.tvd.MC
+
+    solver.bc_lower[0] = pyclaw.BC.wall
+    solver.bc_upper[0] = pyclaw.BC.wall
+    solver.bc_lower[1] = pyclaw.BC.wall
+    solver.bc_upper[1] = pyclaw.BC.wall
+    solver.dimensional_split=1
+
+    #===========================================================================
+    # Initialize domain and state, then initialize the solution associated to the 
+    # state and finally initialize aux array
+    #===========================================================================
+
+    # Domain:
+    xlower = 0.0
+    xupper = 1.0
+    mx = subdivisionFactor
+    ylower = 0.0
+    yupper = 1.0
+    my = subdivisionFactor
+    x = pyclaw.Dimension('x',xlower,xupper,mx)
+    y = pyclaw.Dimension('y',ylower,yupper,my)
+    domain = pyclaw.Domain([x,y])
+
+    num_eqn = 3  # Number of equations
+    state = pyclaw.State(domain,num_eqn)
+
+    grav = 1.0 # Parameter (global auxiliary variable)
+    state.problem_data['grav'] = grav
+
+    # Initial solution
+    # ================
+    # Riemann states of the dam break problem
+    damRadius = 0.5
+    hl = 2.
+    ul = 0.
+    vl = 0.
+    hr = 1.
+    ur = 0.
+    vr = 0.
+    
+    qinit(state,hl,ul,vl,hr,ur,vl,damRadius) # This function is defined above
+
+    #===========================================================================
+    # Set up controller and controller parameters
+    #===========================================================================
+    claw = pyclaw.Controller()
+    claw.tfinal = 2.5
+    claw.solution = pyclaw.Solution(state,domain)
+    claw.solver = peanoSolver
+    claw.outdir = outdir
+    claw.num_output_times = 100
+
+    #===========================================================================
+    # Solve the problem
+    #===========================================================================
+    status = claw.run()
+
+    #===========================================================================
+    # Plot results
+    #===========================================================================
+    if iplot:     plot.interactive_plot(outdir=outdir,format=claw.output_format)
+    if htmlplot:  plot.html_plot(outdir=outdir,format=claw.output_format)
+
+
+if __name__=="__main__":
+    from pyclaw.util import run_app_from_main
+    output = run_app_from_main(shallow2D)
+    print 'Error: ', output
+
+
+
+
+
