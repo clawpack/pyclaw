c04b60aa2142e91963c5116b763e48af677c619d
Aron Ahmadia <aron@ahmadia.net>
2012-04-03 17:08:11 +0300

    scipy-style setup.py files

---
+++ src/petclaw/sharpclaw/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('sharpclaw', parent_package, top_path)
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
---
+++ src/pyclaw/clawpack/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('clawpack', parent_package, top_path)
+
+    config.add_extension('classic1',
+                         ['limiter.f90','philim.f90','step1.f90'])
+
+    config.add_extension('classic2',
+                         ['limiter.f90','philim.f90','flux2.f90','step2ds.f90','step2.f90'])
+
+    config.add_extension('classic3',
+                         ['limiter.f90','philim.f90','flux3.f90','step3ds.f90','step3.f90'])
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
