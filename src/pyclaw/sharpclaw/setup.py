1de09275f1f92a60f9153fb2fdb673186f2f432b
Aron Ahmadia <aron@ahmadia.net>
2012-04-03 17:08:11 +0300

    scipy-style setup.py files

---
+++ src/pyclaw/sharpclaw/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('sharpclaw', parent_package, top_path)
+
+    config.add_extension('sharpclaw1',
+                         ['ClawParams.f90','weno.f90','reconstruct.f90',
+                          'tfluct.f90','evec.f90','workspace.f90','flux1.f90'])
+
+    config.add_extension('sharpclaw2',
+                         ['ClawParams.f90','weno.f90','reconstruct.f90',
+                          'tfluct.f90','evec.f90','workspace2.f90','flux2.f90',
+                          'flux1_2.f90'])
+
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
