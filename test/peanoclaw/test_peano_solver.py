90ecf1b4103a9d372e00067dea765128b42cac43
Kristof Unterweger <unterweg@in.tum.de>
2012-04-10 19:16:59 +0300

    added testcase for peanoclaw

---
+++ test/peanoclaw/test_peano_solver.py
@@ -0,0 +1,23 @@
+from nose.plugins.attrib import attr
+
+if __name__=="__main__":
+    import nose
+    nose.main()
+    
+    
+def test_initialization():
+    from pyclaw.clawpack.clawpack import ClawSolver2D
+    
+    solver = ClawSolver2D()
+    
+    import peanoclaw
+    peano_solver = peanoclaw.Solver(solver, 1.0)
+    
+    import inspect
+    for member in inspect.getmembers(peano_solver):
+        if(not member[0].startswith("_") and not inspect.ismethod(member[1])):
+            print(member[0])
+    for member in inspect.getmembers(solver):
+        if(not member[0].startswith("_") and not inspect.ismethod(member[1])):
+            print(member[0])
+    
