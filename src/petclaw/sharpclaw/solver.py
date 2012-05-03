f0515ab0580c093b937149c7cb3c65bb41da8758
Kyle Mandli <kyle.mandli@gmail.com>
2012-05-02 19:25:16 -0500

    Rename solver modules
    This commit renames the specific solver packages solver modules to the
    more generic name solver.  In other words the module containing the
    solver definitions will always be in a module within a specific package
    called solver.

--- src/petclaw/sharpclaw/solver.py
+++ src/petclaw/sharpclaw/solver.py
@@ -6,12 +6,12 @@ Module containing SharpClaw solvers for PetClaw
 import pyclaw.sharpclaw.sharpclaw
 
 
-class SharpClawSolver1D(pyclaw.sharpclaw.sharpclaw.SharpClawSolver1D):
+class SharpClawSolver1D(pyclaw.sharpclaw.solver.SharpClawSolver1D):
     """
     1D parallel SharpClaw solver.  See the corresponding PyClaw class for documentation.
     """
 
-class SharpClawSolver2D(pyclaw.sharpclaw.sharpclaw.SharpClawSolver2D):
+class SharpClawSolver2D(pyclaw.sharpclaw.solver.SharpClawSolver2D):
     """
     2D parallel SharpClaw solver.  See the corresponding PyClaw class for documentation.
     """
