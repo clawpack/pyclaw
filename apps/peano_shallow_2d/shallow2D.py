e77c0db41dae8449889ad984e3228ae7545a45e3
Kristof Unterweger <unterweg@in.tum.de>
2012-05-15 11:13:04 +0200

    current state

--- apps/peano_shallow_2d/shallow2D.py
+++ apps/peano_shallow_2d/shallow2D.py
@@ -9,20 +9,32 @@
 #===========================================================================
 
 import numpy as np
-from petclaw import plot
+#from petclaw import plot
 #import pdb  # Debugger
 
-
-def qinit(state,hl,ul,vl,hr,ur,vr,radDam):
+def init(state):
+    # Initial solution
+    # ================
+    # Riemann states of the dam break problem
+    radDam = 0.2
+    hl = 2.
+    ul = 0.
+    vl = 0.
+    hr = 1.
+    ur = 0.
+    vr = 0.
+    
     x0=0.5
     y0=0.5
     xCenter = state.grid.x.centers
     yCenter = state.grid.y.centers
+    
     Y,X = np.meshgrid(yCenter,xCenter)
     r = np.sqrt((X-x0)**2 + (Y-y0)**2)
     state.q[0,:,:] = hl*(r<=radDam) + hr*(r>radDam)
     state.q[1,:,:] = hl*ul*(r<=radDam) + hr*ur*(r>radDam)
     state.q[2,:,:] = hl*vl*(r<=radDam) + hr*vr*(r>radDam)
+
 
     
 def shallow2D(use_petsc=False,iplot=0,htmlplot=False,outdir='./_output',solver_type='classic'):
@@ -47,7 +59,7 @@ def shallow2D(use_petsc=False,iplot=0,htmlplot=False,outdir='./_output',solver_t
         solver.dimensional_split=1
     elif solver_type == 'sharpclaw':
         solver = pyclaw.SharpClawSolver2D()
-    peanoSolver = peanoclaw.Solver(solver, (1./9.)/subdivisionFactor)
+    peanoSolver = peanoclaw.Solver(solver, (1./3.)/subdivisionFactor, init)
     
     solver.dt_initial = 1.0
 
@@ -81,20 +93,7 @@ def shallow2D(use_petsc=False,iplot=0,htmlplot=False,outdir='./_output',solver_t
 
     grav = 1.0 # Parameter (global auxiliary variable)
     state.problem_data['grav'] = grav
-
-    # Initial solution
-    # ================
-    # Riemann states of the dam break problem
-    damRadius = 0.2
-    hl = 2.
-    ul = 0.
-    vl = 0.
-    hr = 1.
-    ur = 0.
-    vr = 0.
     
-    qinit(state,hl,ul,vl,hr,ur,vl,damRadius) # This function is defined above
-
     #===========================================================================
     # Set up controller and controller parameters
     #===========================================================================
