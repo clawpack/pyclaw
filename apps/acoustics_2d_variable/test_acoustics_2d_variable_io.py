0121324b165f6046ba9f61f95e1c25a5c00ba86e
Amal Alghamdi <amal.m.alghamdi@gmail.com>
2013-02-18 09:20:51 +0300

    read test results from file

--- apps/acoustics_2d_variable/test_acoustics_2d_variable_io.py
+++ apps/acoustics_2d_variable/test_acoustics_2d_variable_io.py
@@ -4,29 +4,40 @@ def test_acoustics_2d_variable_io():
     from acoustics import acoustics2D
 
     def verify_acoustics_io(controller):
+    """ Verifies I/O on 2d variable-coefficient acoustics application"""
         import os
         from clawpack.pyclaw.util import check_diff
         import numpy as np
         from  pyclaw import Solution
-        """ Verifies I/O on 2d variable-coefficient acoustics application"""
+        
         thisdir = os.path.dirname(__file__)
         verify_dir = os.path.join(thisdir,'./io_test_verification')
         
-        sol_0 = Solution()
-        sol_0.read(0,path=verify_dir,file_format='ascii',
+        # Expected solution
+        sol_0_expected = Solution()
+        sol_0_expected.read(0,path=verify_dir,file_format='ascii',
                                file_prefix=None,read_aux=True)
-        expected_aux = sol_0.state.aux
+        expected_aux = sol_0_expected.state.aux
 
-        sol_20 = Solution()
-        sol_20.read(20,path=verify_dir,file_format='ascii',
+        sol_20_expected = Solution()
+        sol_20_expected.read(20,path=verify_dir,file_format='ascii',
                                file_prefix=None,read_aux=False)
-        expected_q = sol_20.state.q
+        expected_q = sol_20_expected.state.q
+
+        # Test solution
+        sol_0_test = Solution()
+        sol_0_test.read(0,path=controller.outdir,
+                        file_format=controller.output_format,
+                        file_prefix=None,read_aux=True)
+        test_aux = sol_0_test.state.get_aux_global()
+
+        sol_20_test = Solution()
+        sol_20_test.read(20,path=controller.outdir,
+                        file_format=controller.output_format,
+                        file_prefix=None,read_aux=False)
+        test_q = sol_20_test.state.get_q_global()
 
 
-        state = controller.frames[controller.num_output_times].state
-        test_q=state.get_q_global()
-        test_aux = state.get_aux_global()
-        
         test_passed = True
         if test_q is not None:
             q_err = check_diff(expected_q, test_q, reltol=1e-4)
@@ -51,7 +62,7 @@ def test_acoustics_2d_variable_io():
     from clawpack.pyclaw.util import gen_variants
 
     classic_tests = gen_variants(acoustics2D, verify_acoustics_io,
-                                 solver_type='classic')
+                                 solver_type='classic', outdir='./_io_test_results')
 
 
     from itertools import chain
