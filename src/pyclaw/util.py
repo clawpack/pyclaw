55d48965cec3743ea8f13ef4f026c0f3ac47ef4b
Aron Ahmadia <aron@ahmadia.net>
2013-04-07 15:00:45 +0200

    improved check_diff reporting

--- src/pyclaw/util.py
+++ src/pyclaw/util.py
@@ -42,21 +42,21 @@ def gen_variants(application, verifier, kernel_languages=('Fortran',), **kwargs)
     r"""
     Generator of runnable variants of a test application given a verifier
 
-    Given an application, a script for verifying its output, and a 
+    Given an application, a script for verifying its output, and a
     list of kernel languages to try, generates all possible variants of the
-    application to try by taking a product of the available kernel_languages and 
-    (petclaw/pyclaw).  For many applications, this will generate 4 variants: 
+    application to try by taking a product of the available kernel_languages and
+    (petclaw/pyclaw).  For many applications, this will generate 4 variants:
     the product of the two main kernel languages ('Fortran' and 'Python'), against
-    the the two parallel modes (petclaw and pyclaw).  
+    the the two parallel modes (petclaw and pyclaw).
 
-    For more information on how the verifier function should be implemented, 
+    For more information on how the verifier function should be implemented,
     see util.test_app for a description, and util.check_diff for an example.
 
     All unrecognized keyword arguments are passed through to the application.
     """
 
     arg_dicts = build_variant_arg_dicts(kernel_languages)
-    
+
     for test_kwargs in arg_dicts:
         test_kwargs.update(kwargs)
         yield (test_app, application, verifier, test_kwargs)
@@ -92,7 +92,7 @@ def test_app(application, verifier, kwargs):
     Test the output of a given application against its verifier method.
 
     This function performs the following two function calls::
- 
+
         output = application(**kwargs)
         check_values = verifier(output)
 
@@ -118,10 +118,10 @@ def test_app(application, verifier, kwargs):
                 return
         except ImportError, e:
             pass
-    
+
     output = application(**kwargs)
     check_values = verifier(output)
-    
+
     if check_values is not None:
         import inspect
         err = \
@@ -131,6 +131,7 @@ verification function
 %s
 args                 : %s
 norm of expected data: %s
+norm of test data    : %s
 test error           : %s
 %s
 ********************************************************************************
@@ -140,7 +141,8 @@ test error           : %s
          kwargs,
          check_values[0],
          check_values[1],
-         check_values[2])
+         check_values[2],
+         check_values[3])
         raise VerifyError(err)
     return
 
@@ -152,12 +154,15 @@ def check_diff(expected, test, **kwargs):
     """
     err_norm = np.linalg.norm(expected - test)
     expected_norm = np.linalg.norm(expected)
+    test_norm = np.linalg.norm(test)
     if 'abstol' in kwargs:
         if err_norm < kwargs['abstol']: return None
-        else: return (expected_norm, err_norm, 'abstol  : %s' % kwargs['abstol'])
+        else: return (expected_norm, test_norm, err_norm,
+                      'abstol  : %s' % kwargs['abstol'])
     elif 'reltol' in kwargs:
         if err_norm/expected_norm < kwargs['reltol']: return None
-        else: return (expected_norm, err_norm, 'reltol  : %s' % kwargs['reltol'])
+        else: return (expected_norm, test_norm, err_norm,
+                      'reltol  : %s' % kwargs['reltol'])
     else:
         raise Exception('Incorrect use of check_diff verifier, specify tol!')
 
@@ -171,94 +176,94 @@ def compile_library(source_list,module_name,interface_functions=[],
                         FC=None,FFLAGS=None,recompile=False,clean=False):
     r"""
     Compiles and wraps fortran source into a callable module in python.
-    
+
     This function uses f2py to create an interface from python to the fortran
     sources in source_list.  The source_list can either be a list of names
     of source files in which case compile_library will search for the file in
     local_path and then in library_path.  If a path is given, the file will be
     checked to see if it exists, if not it will look for the file in the above
     resolution order.  If any source file is not found, an IOException is
-    raised.  
-    
-    The list interface_functions allows the user to specify which fortran 
+    raised.
+
+    The list interface_functions allows the user to specify which fortran
     functions are actually available to python.  The interface functions are
     assumed to be in the file with their name, i.e. claw1 is located in
     'claw1.f95' or 'claw1.f'.
-    
+
     The interface from fortran may be different than the original function
-    call in fortran so the user should make sure to check the automatically 
+    call in fortran so the user should make sure to check the automatically
     created doc string for the fortran module for proper use.
-    
+
     Source files will not be recompiled if they have not been changed.
-    
+
     One set of options of note is for enabling OpenMP, it requires the usual
     fortran flags but the OpenMP library also must be compiled in, this is
     done with the flag -lgomp.  The call to compile_library would then be:
-    
+
     compile_library(src,module_name,f2py_flags='-lgomp',FFLAGS='-fopenmp')
-    
+
     For complete optimization use:
-    
+
     FFLAGS='-O3 -fopenmp -funroll-loops -finline-functions -fdefault-real-8'
-    
+
     :Input:
-     - *source_list* - (list of strings) List of source files, if these are 
+     - *source_list* - (list of strings) List of source files, if these are
        just names of the source files, i.e. 'bc1.f' then they will be searched
-       for in the default source resolution order, if an explicit path is 
+       for in the default source resolution order, if an explicit path is
        given, i.e. './bc1.f', then the function will use that source if it can
        find it.
      - *module_name* - (string) Name of the resulting module
-     - *interface_functions* - (list of strings) List of function names to 
-       provide access to, if empty, all functions are accessible to python.  
+     - *interface_functions* - (list of strings) List of function names to
+       provide access to, if empty, all functions are accessible to python.
        Defaults to [].
-     - *local_path* - (string) The base path for source resolution, defaults 
+     - *local_path* - (string) The base path for source resolution, defaults
        to './'.
-     - *library_path* - (string) The library path for source resolution, 
+     - *library_path* - (string) The library path for source resolution,
        defaults to './'.
      - *f2py_flags* - (string) f2py flags to be passed
-     - *FC* - (string) Override the environment variable FC and use it to 
-       compile, note that this does not replace the compiler that f2py uses, 
-       only the object file compilation (functions that do not have 
+     - *FC* - (string) Override the environment variable FC and use it to
+       compile, note that this does not replace the compiler that f2py uses,
+       only the object file compilation (functions that do not have
        interfaces)
-     - *FFLAGS* - (string) Override the environment variable FFLAGS and pass 
+     - *FFLAGS* - (string) Override the environment variable FFLAGS and pass
        them to the fortran compiler
-     - *recompile* - (bool) Force recompilation of the library, defaults to 
+     - *recompile* - (bool) Force recompilation of the library, defaults to
        False
      - *clean* - (bool) Force a clean build of all source files
     """
-    
-    # Setup logger    
+
+    # Setup logger
     logger = logging.getLogger('f2py')
     temp_file = tempfile.TemporaryFile()
     logger.info('Compiling %s' % module_name)
-    
+
     # Force recompile if the clean flag is set
     if clean:
         recompile = True
-    
+
     # Expand local_path and library_path
     local_path = os.path.expandvars(local_path)
     local_path = os.path.expanduser(local_path)
     library_path = os.path.expandvars(library_path)
     library_path = os.path.expanduser(library_path)
-    
+
     # Fetch environment variables we need for compilation
     if FC is None:
         if os.environ.has_key('FC'):
             FC = os.environ['FC']
         else:
             FC = 'gfortran'
-      
-    if FFLAGS is None:          
+
+    if FFLAGS is None:
         if os.environ.has_key('FFLAGS'):
             FFLAGS = os.environ['FFLAGS']
         else:
             FFLAGS = ''
-    
+
     # Create the list of paths to sources
     path_list = []
     for source in source_list:
-        # Check to see if the source looks like a path, i.e. it contains the 
+        # Check to see if the source looks like a path, i.e. it contains the
         # os.path.sep character
         if source.find(os.path.sep) >= 0:
             source = os.path.expandvars(source)
@@ -270,7 +275,7 @@ def compile_library(source_list,module_name,interface_functions=[],
             # Otherwise, take the last part of the path and try searching for
             # it in the resolution order
             source = os.path.split(source)
-        
+
         # Search for the source file in local_path and then library_path
         if os.path.exists(os.path.join(local_path,source)):
             path_list.append(os.path.join(local_path,source))
@@ -280,7 +285,7 @@ def compile_library(source_list,module_name,interface_functions=[],
             continue
         else:
             raise IOError('Could not find source file %s' % source)
-            
+
     # Compile each of the source files if the object files are not present or
     # if the modification date of the source file is newer than the object
     # file's creation date
@@ -289,7 +294,7 @@ def compile_library(source_list,module_name,interface_functions=[],
     for path in path_list:
         object_path = os.path.join(os.path.split(path)[0],
             '.'.join((os.path.split(path)[1].split('.')[:-1][0],'o')))
-        
+
         # Check to see if this path contains one of the interface functions
         if os.path.split(path)[1].split('.')[:-1][0] in interface_functions:
             src_list.append(path)
@@ -299,7 +304,7 @@ def compile_library(source_list,module_name,interface_functions=[],
         elif len(interface_functions) == 0:
             src_list.append(path)
             continue
-            
+
         if os.path.exists(object_path) and not clean:
             # Check to see if the modification date of the source file is
             # greater than the object file
@@ -311,7 +316,7 @@ def compile_library(source_list,module_name,interface_functions=[],
         logger.debug(command)
         subprocess.call(command,shell=True,stdout=temp_file)
         object_list.append(object_path)
-        
+
     # Check to see if recompile is needed
     if not recompile:
         module_path = os.path.join('.','.'.join((module_name,'so')))
@@ -323,7 +328,7 @@ def compile_library(source_list,module_name,interface_functions=[],
             for obj in object_list:
                 if os.path.getmtime(module_path) < os.path.getmtime(obj):
                     recompile = True
-                    break  
+                    break
         else:
             recompile = True
 
@@ -362,16 +367,16 @@ def compile_library(source_list,module_name,interface_functions=[],
 def construct_function_handle(path,function_name=None):
     r"""
     Constructs a function handle from the file at path.
-    
+
     This function will attempt to construct a function handle from the python
     file at path.
-    
+
     :Input:
      - *path* - (string) Path to the file containing the function
-     - *function_name* - (string) Name of the function defined in the file 
-       that the handle will point to.  Defaults to the same name as the file 
+     - *function_name* - (string) Name of the function defined in the file
+       that the handle will point to.  Defaults to the same name as the file
        without the extension.
-       
+
     :Output:
      - (func) Function handle to the constructed function, None if this has
        failed.
@@ -379,7 +384,7 @@ def construct_function_handle(path,function_name=None):
     # Determine the resulting function_name
     if function_name is None:
         function_name = path.split('/')[-1].split('.')[0]
-    
+
     full_path = os.path.abspath(path)
     if os.path.exists(full_path):
         suffix = path.split('.')[-1]
@@ -420,7 +425,7 @@ def read_data_line(inputfile,num_entries=1,type='float'):
     try:
         for i in range(num_entries):
             exec("val[i] = %s(l[i])" % type)
-        if num_entries == 1:  # This is a convenience for calling functions    
+        if num_entries == 1:  # This is a convenience for calling functions
             return val[0]
         return val
     except(ValueError):
@@ -438,7 +443,7 @@ def convert_fort_double_to_float(number):
 
     Converts a fortran format double to a python float.
 
-    number: is a string representation of the double.  Number should 
+    number: is a string representation of the double.  Number should
     be of the form "1.0d0"
 
     """
@@ -461,7 +466,7 @@ def current_time(addtz=False):
 
 def _method_info_from_argv(argv=None):
     """Command-line -> method call arg processing.
-    
+
     - positional args:
             a b -> method('a', 'b')
     - intifying args:
@@ -472,7 +477,7 @@ def _method_info_from_argv(argv=None):
             a foo=bar -> method('a', foo='bar')
     - using more of the above
             1234 'extras=["r2"]'  -> method(1234, extras=["r2"])
-    
+
     @param argv {list} Command line arg list. Defaults to `sys.argv`.
     @returns (<method-name>, <args>, <kwargs>)
     """
@@ -489,7 +494,7 @@ def _method_info_from_argv(argv=None):
         else:
             key, value = None, s
         try:
-            value = json.loads(value) 
+            value = json.loads(value)
         except ValueError:
             pass
         if value=='True': value=True
@@ -502,7 +507,7 @@ def _method_info_from_argv(argv=None):
 
 def _info_from_argv(argv=None):
     """Command-line -> method call arg processing.
-    
+
     - positional args:
             a b -> method('a', 'b')
     - intifying args:
@@ -513,7 +518,7 @@ def _info_from_argv(argv=None):
             a foo=bar -> method('a', foo='bar')
     - using more of the above
             1234 'extras=["r2"]'  -> method(1234, extras=["r2"])
-    
+
     @param argv {list} Command line arg list. Defaults to `sys.argv`.
     @returns (<method-name>, <args>, <kwargs>)
     """
@@ -530,7 +535,7 @@ def _info_from_argv(argv=None):
         else:
             key, value = None, s
         try:
-            value = json.loads(value) 
+            value = json.loads(value)
         except ValueError:
             pass
         if value=='True': value=True
@@ -562,7 +567,7 @@ class FrameCounter:
     Simple frame counter
 
     Simple frame counter to keep track of current frame number.  This can
-    also be used to keep multiple runs frames seperated by having multiple 
+    also be used to keep multiple runs frames seperated by having multiple
     counters at once.
 
     Initializes to 0
@@ -593,5 +598,3 @@ class FrameCounter:
         Reset the counter to 0
         """
         self.__frame = 0
-
-
