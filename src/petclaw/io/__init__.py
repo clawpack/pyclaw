08ce7b3c26f0df16ed9a700337c25ea923c4f1c4
David Ketcheson <dketch@gmail.com>
2013-07-25 15:14:07 -0700

    Use pythonic names for io functions.

--- src/petclaw/io/__init__.py
+++ src/petclaw/io/__init__.py
@@ -9,30 +9,29 @@
 """Output package for Pyclaw"""
 
 import logging
-from clawpack.pyclaw.io.ascii import read_ascii,write_ascii
-__all__ = ['read_ascii','write_ascii']
+from clawpack.pyclaw.io import ascii
+__all__ = ['ascii.read','ascii.write']
 
 # Check for HDF 5 support
 try:
     import h5py
-    from clawpack.pyclaw.io.hdf5 import read_hdf5,write_hdf5
-    __all__ += ['read_hdf5','write_hdf5']
+    from clawpack.pyclaw.io.hdf5 import hdf5.read,hdf5.write
+    __all__ += ['hdf5.read','hdf5.write']
 except:
     logging.debug("No hdf5 support found.")
     
 # Check for netcdf support
 try:
     import netCDF4
-    from clawpack.pyclaw.io.netcdf import read_netcdf, write_netcdf
-    __all__ += ['read_netcdf','write_netcdf']
+    from clawpack.pyclaw.io.netcdf import netcdf.read, netcdf.write
+    __all__ += ['netcdf.read','netcdf.write']
 except(ImportError):
     logging.debug("No netcdf4 support found.")
 
 # Check for petsc4py support
 try:
     import petsc
-    from petsc import read_petsc, write_petsc
-    __all__ += ['read_petsc','write_petsc']
+    __all__ += ['petsc.read','petsc.write']
 except(ImportError):
     logging.debug("No petsc support found.")
  
