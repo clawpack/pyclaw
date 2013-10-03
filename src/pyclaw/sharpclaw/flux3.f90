16fa1c4e4d9c04594f2ae294767101a8b3d87f85
David Ketcheson <dketch@gmail.com>
2013-10-03 09:09:13 +0300

    Remove useless comments about capa in sharpclaw flux3.

--- src/pyclaw/sharpclaw/flux3.f90
+++ src/pyclaw/sharpclaw/flux3.f90
@@ -42,30 +42,19 @@ subroutine flux3(q,dq,q1d,dq1d,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,m
    
     do j = 0,my+1
         do k = 0,mz+1
-            ! copy auxiliary data along a slice into 1d arrays:
+            ! copy data along a slice into 1d arrays:
             q1dp => q(:,:,j,k)
             if (num_aux .gt. 0)  then
                 auxp => aux(:,:,j,k)
             endif
 
-
             ! compute modification dq1d along this slice:
             call flux1(q1dp,dq1d,auxp,dt,cfl1d,t,1,num_aux,num_eqn,mx,num_ghost,maxnx,rpn3)
             cfl = dmax1(cfl,cfl1d)
 
-
-            if (index_capa.eq.0) then
-                ! no capa array.  Standard flux differencing:
-                forall(i=1:mx, m=1:num_eqn)
-                    dq(m,i,j,k) = dq(m,i,j,k)+dq1d(m,i)
-                end forall
-            else
-                ! with capa array.  Which is correct?
-                forall(i=1:mx, m=1:num_eqn)
-                    dq(m,i,j,k) = dq(m,i,j,k)+dq1d(m,i)
-                end forall
-                ! dq(m,i,j) = dq(m,i,j)+dq1d(m,i)/aux(index_capa,i,j)
-            endif
+            forall(i=1:mx, m=1:num_eqn)
+                dq(m,i,j,k) = dq(m,i,j,k)+dq1d(m,i)
+            end forall
         enddo
     enddo !end x sweeps
 
@@ -75,12 +64,8 @@ subroutine flux3(q,dq,q1d,dq1d,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,m
 
     do i = 0, mx+1
         do k = 0, mz+1
-            ! copy auxiliary data along a slice into 1d arrays:
+            ! copy data along a slice into 1d arrays:
             q1dp => q(:,i,:,k)
-            forall(j=1-num_ghost:my+num_ghost, m=1:num_eqn)
-                q1d(m,j) = q(m,i,j,k)
-            end forall
-
             if (num_aux .gt. 0)  then
                 auxp => aux(:,i,:,k)
             endif
@@ -88,16 +73,9 @@ subroutine flux3(q,dq,q1d,dq1d,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,m
             call flux1(q1dp,dq1d,auxp,dt,cfl1d,t,2,num_aux,num_eqn,my,num_ghost,maxnx,rpn3)
             cfl = dmax1(cfl,cfl1d)
 
-            if (index_capa.eq.0) then
-                ! no capa array.  Standard flux differencing:
-                forall(j=1:my,m=1:num_eqn)
-                    dq(m,i,j,k) = dq(m,i,j,k)+dq1d(m,j)
-                end forall
-            else
-                ! with capa array.  Which is correct?
-                ! dq(m,i,j) = dq(m,i,j)+dq1d(m,j)/aux(index_capa,i,j)
-                dq(:,i,:,k) = dq(:,i,:,k)+dq1d
-            endif
+            forall(j=1:my,m=1:num_eqn)
+                dq(m,i,j,k) = dq(m,i,j,k)+dq1d(m,j)
+            end forall
         enddo
     enddo !end y sweeps
 
@@ -106,12 +84,8 @@ subroutine flux3(q,dq,q1d,dq1d,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,m
 
     do i = 0, mx+1
         do j = 0, my+1
-            ! copy auxiliary data along a slice into 1d arrays:
+            ! copy data along a slice into 1d arrays:
             q1dp => q(:,i,j,:)
-            forall(k=1-num_ghost:mz+num_ghost, m=1:num_eqn)
-                q1d(m,k) = q(m,i,j,k)
-            end forall
-
             if (num_aux .gt. 0)  then
                 auxp => aux(:,i,j,:)
             endif
@@ -119,16 +93,9 @@ subroutine flux3(q,dq,q1d,dq1d,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,m
             call flux1(q1dp,dq1d,auxp,dt,cfl1d,t,3,num_aux,num_eqn,mz,num_ghost,maxnx,rpn3)
             cfl = dmax1(cfl,cfl1d)
 
-            if (index_capa.eq.0) then
-                ! no capa array.  Standard flux differencing:
-                forall(k=1:mz,m=1:num_eqn)
-                    dq(m,i,j,k) = dq(m,i,j,k)+dq1d(m,k)
-                end forall
-            else
-                ! with capa array.  Which is correct?
-                ! dq(m,i,j) = dq(m,i,j)+dq1d(m,j)/aux(index_capa,i,j)
-                dq(:,i,j,:) = dq(:,i,j,:)+dq1d
-            endif
+            forall(k=1:mz,m=1:num_eqn)
+                dq(m,i,j,k) = dq(m,i,j,k)+dq1d(m,k)
+            end forall
         enddo
     enddo !end y sweeps
 
