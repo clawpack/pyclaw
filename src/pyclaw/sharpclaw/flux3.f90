b7cb6559192d7c2bc6204d5e362e3a1f51ad35d2
damiansra <damiansra@gmail.com>
2013-11-06 13:46:24 +0300

    corrected typos and added missing arguments

--- src/pyclaw/sharpclaw/flux3.f90
+++ src/pyclaw/sharpclaw/flux3.f90
@@ -1,5 +1,5 @@
 ! ==========================================================
-subroutine flux3(q,dq,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,my,rpn3)
+subroutine flux3(q,dq,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,my,mz,rpn3)
 ! ==========================================================
 
     ! Evaluate (delta t) *dq/dt
@@ -10,30 +10,30 @@ subroutine flux3(q,dq,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,my,rpn3)
     ! dq1d is used to return increments to q from flux1
     ! See the flux1 documentation for more information.
 
-    use workspace
     use ClawParams
+    use workspace
     implicit none
 
     external :: rpn3
     integer, intent(in) :: num_aux,num_eqn,num_ghost,maxnx,mx,my,mz
     double precision, intent(in) :: dt,t
-    double precision, target, intent(in) :: q(num_eqn, 1-num_ghost:mx+num_ghost, 1-num_ghost:my+num_ghost,1-num_ghost:mz+num_ghost)
+    double precision, target, intent(in) :: q(num_eqn,1-num_ghost:mx+num_ghost,1-num_ghost:my+num_ghost,1-num_ghost:mz+num_ghost)
     double precision, target, intent(in) :: aux(num_aux,1-num_ghost:mx+num_ghost,1-num_ghost:my+num_ghost,1-num_ghost:mz+num_ghost)
-    double precision, intent(inout) :: dq(num_eqn, 1-num_ghost:mx+num_ghost, 1-num_ghost:my+num_ghost,1-num_ghost:mz+num_ghost)
+    double precision, intent(inout) :: dq(num_eqn,1-num_ghost:mx+num_ghost,1-num_ghost:my+num_ghost,1-num_ghost:mz+num_ghost)
     double precision, intent(out) :: cfl
 
-    !f2py intent(in,out) dq  
-    !f2py intent(out) cfl  
-    !f2py optional dq
+!f2py intent(in,out) dq  
+!f2py intent(out) cfl  
+!f2py optional dq
 
-    ! Dummy interface just so f2py doesn't complain:
-    !f2py real(DP) x
-    !f2py x=rpn3(x)
+! Dummy interface just so f2py doesn't complain:
+!f2py real(DP) x
+!f2py x=rpn3(x)
     
     !local variables
     integer :: i,j,k,m
     double precision :: cfl1d
-    double precision, pointer :: aux1d(:,:),q1dp(:,:)
+    double precision, pointer :: aux1d(:,:),q1d(:,:)
     double precision :: dq1d(num_eqn,1-num_ghost:maxnx+num_ghost)
     
     cfl = 0.d0
@@ -45,11 +45,11 @@ subroutine flux3(q,dq,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,my,rpn3)
     do j = 0,my+1
         do k = 0,mz+1
             ! copy data along a slice into 1d arrays:
-            q1dp => q(:,:,j,k)
+            q1d => q(:,:,j,k)
             aux1d => aux(:,:,j,k)
 
             ! compute modification dq1d along this slice:
-            call flux1(q1dp,dq1d,aux1d,dt,cfl1d,t,1,num_aux,num_eqn,mx,num_ghost,maxnx,rpn3)
+            call flux1(q1d,dq1d,aux1d,dt,cfl1d,t,1,num_aux,num_eqn,mx,num_ghost,maxnx,rpn3)
             cfl = dmax1(cfl,cfl1d)
 
             forall(i=1:mx, m=1:num_eqn)
@@ -65,10 +65,10 @@ subroutine flux3(q,dq,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,my,rpn3)
     do i = 0, mx+1
         do k = 0, mz+1
             ! copy data along a slice into 1d arrays:
-            q1dp => q(:,i,:,k)
+            q1d => q(:,i,:,k)
             aux1d => aux(:,i,:,k)
 
-            call flux1(q1dp,dq1d,aux1d,dt,cfl1d,t,2,num_aux,num_eqn,my,num_ghost,maxnx,rpn3)
+            call flux1(q1d,dq1d,aux1d,dt,cfl1d,t,2,num_aux,num_eqn,my,num_ghost,maxnx,rpn3)
             cfl = dmax1(cfl,cfl1d)
 
             forall(j=1:my,m=1:num_eqn)
@@ -83,10 +83,10 @@ subroutine flux3(q,dq,aux,dt,cfl,t,num_aux,num_eqn,num_ghost,maxnx,mx,my,rpn3)
     do i = 0, mx+1
         do j = 0, my+1
             ! copy data along a slice into 1d arrays:
-            q1dp => q(:,i,j,:)
+            q1d => q(:,i,j,:)
             aux1d => aux(:,i,j,:)
 
-            call flux1(q1dp,dq1d,aux1d,dt,cfl1d,t,3,num_aux,num_eqn,mz,num_ghost,maxnx,rpn3)
+            call flux1(q1d,dq1d,aux1d,dt,cfl1d,t,3,num_aux,num_eqn,mz,num_ghost,maxnx,rpn3)
             cfl = dmax1(cfl,cfl1d)
 
             forall(k=1:mz,m=1:num_eqn)
