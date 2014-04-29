d64ec40b95c4f14ffbcbc6b74f1155f6f302e058
Matthew Emmett <mwemmett@lbl.gov>
2014-04-29 09:54:37 -0700

    BoxClaw: Tidy (remove commented block).

--- src/boxclaw/plot.py
+++ src/boxclaw/plot.py
@@ -11,27 +11,3 @@ def html_plot(outdir='./_output',file_format='ascii',setplot=None):
     """
     from clawpack.pyclaw.plot import plot
     plot(setplot,outdir=outdir,file_format=file_format,htmlplot=True,iplot=False)
-
-# def plotPetsc(clawobj,delay=1):
-#     """
-#     Takes either a controller or solution object and prints each frame
-#     using PETSc.Viewer.
-#     """
-#     from petsc4py import PETSc
-#     from clawpack.pyclaw import controller, solution
-
-#     if isinstance(clawobj,controller.Controller):
-#         for n in xrange(0,clawobj.num_output_times):
-#             sol = clawobj.frames[n]
-#             viewer = PETSc.Viewer.DRAW(sol.patch.gqVec.comm)
-#             OptDB = PETSc.Options()
-#             OptDB['draw_pause'] = delay
-#             viewer(sol.patch.gqVec)
-
-#     elif isinstance(clawobj,solution.Solution):
-#         viewer = PETSc.Viewer.DRAW(clawobj.patch.gqVec.comm)
-#         OptDB = PETSc.Options()
-#         OptDB['draw_pause'] = -1
-#         viewer(clawobj.patch.gqVec)
-
-
