5b9d625f1048db749bddfd2ca402be4ba9b84752
Aron Ahmadia <aron@ahmadia.net>
2014-05-05 21:32:30 -0500

    Parallel petclaw tests in Travis

---
+++ .travis_build_petsc4py.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Get <#>
+git clone https://github.com/hashdist/hashdist
+export PATH=`pwd`/hashdist/bin:$PATH
+
+# a reasonable profile for building petsc4py on Travis
+cp ../.hashdist_petsc4py_stack.yaml petsc4py_stack.yaml
+
+# This monitors the slow source-based Python/PETSc/petsc4py install
+# Will be much faster with relocatable binary builds when available from <#>
+set -o pipefail
+hit develop -v petsc4py_stack.yaml 2>&1 | pv -i 15 -n | cat > log.txt
+tail -n 60 log.txt
