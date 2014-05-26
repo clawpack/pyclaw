d8404dba9c65a64f22aab56228871eb940f176d4
Aron Ahmadia <aron@ahmadia.net>
2014-05-26 16:34:46 -0400

    Clean up 3D acoustics test

--- examples/acoustics_3d_variable/test_3d_acoustics.py
+++ examples/acoustics_3d_variable/test_3d_acoustics.py
@@ -1,63 +1,69 @@
+import os
+from itertools import chain
+
+import numpy as np
+
+from clawpack.pyclaw.util import gen_variants
+from clawpack.pyclaw.util import check_diff
+
+import acoustics_3d_interface
+
+
 def test_3d_acoustics():
-    """ test_3d_acoustics """
+    """ Tests for homogeneous and heterogeneous 3D acoustics"""
 
     def acoustics_verify_homogeneous(claw):
-        from clawpack.pyclaw.util import check_diff
-        import numpy as np
+        """ Regression test for 3D homogeneous acoustics equations.
+        """
 
         pinitial = claw.frames[0].state.get_q_global()
-        pfinal   = claw.frames[claw.num_output_times].state.get_q_global()
+        pfinal = claw.frames[claw.num_output_times].state.get_q_global()
 
-        pinitial = pinitial[0,:,:,:].reshape(-1)
-        pfinal   = pfinal[0,:,:,:].reshape(-1)
-        grid = claw.solution.state.grid
-        final_difference =np.prod(grid.delta)*np.linalg.norm(pfinal-pinitial,ord=1)
-
-        return check_diff(0., final_difference, abstol=1e-1)
+        if pinitial is not None and pfinal is not None:
+            pinitial = pinitial[0, :, :, :].reshape(-1)
+            pfinal = pfinal[0, :, :, :].reshape(-1)
+            grid = claw.solution.state.grid
+            final_difference = np.prod(grid.delta)*np.linalg.norm(pfinal-pinitial, ord=1)
+            return check_diff(0., final_difference, abstol=1e-1)
+        else:
+            return
 
     def acoustics_verify_heterogeneous(claw):
-        import os
-        import numpy as np
-        from clawpack.pyclaw.util import check_diff
+        """ Regression test for 3D heterogeneous acoustics equations
+        """
 
         pinitial = claw.frames[0].state.get_q_global()
-        pfinal   = claw.frames[claw.num_output_times].state.get_q_global()
+        pfinal = claw.frames[claw.num_output_times].state.get_q_global()
 
-        pinitial = pinitial[0,:,:,:].reshape(-1)
-        pfinal   = pfinal[0,:,:,:].reshape(-1)
-        grid = claw.solution.state.grid
-        final_difference =np.prod(grid.delta)*np.linalg.norm(pfinal-pinitial,ord=1)
+        if pinitial is not None and pfinal is not None:
+            pfinal = pfinal[0, :, :, :].reshape(-1)
+            thisdir = os.path.dirname(__file__)
+            verify_pfinal = np.loadtxt(os.path.join(thisdir, 'verify_classic_heterogeneous.txt'))
+            norm_err = np.linalg.norm(pfinal-verify_pfinal)
+            return check_diff(0, norm_err, abstol=10.)
+        else:
+            return
 
-        thisdir = os.path.dirname(__file__)
-        verify_pfinal = np.loadtxt(os.path.join(thisdir,'verify_classic_heterogeneous.txt'))
-        norm_err = np.linalg.norm(pfinal-verify_pfinal)
-        return check_diff(0, norm_err, abstol=10.)
+    classic_homogeneous_tests = gen_variants(acoustics_3d_interface.setup, acoustics_verify_homogeneous,
+                                             kernel_languages=('Fortran',),
+                                             solver_type='classic', test='homogeneous',
+                                             disable_output=True)
 
+    classic_heterogeneous_tests = gen_variants(acoustics_3d_interface.setup, acoustics_verify_heterogeneous,
+                                               kernel_languages=('Fortran',),
+                                               solver_type='classic', test='heterogeneous',
+                                               disable_output=True)
 
+    sharp_homogeneous_tests = gen_variants(acoustics_3d_interface.setup, acoustics_verify_homogeneous,
+                                           kernel_languages=('Fortran',),
+                                           solver_type='sharpclaw', test='homogeneous',
+                                           disable_output=True)
 
-    from clawpack.pyclaw.util import gen_variants
-    import acoustics_3d_interface
+    sharp_heterogeneous_tests = gen_variants(acoustics_3d_interface.setup, acoustics_verify_heterogeneous,
+                                             kernel_languages=('Fortran',),
+                                             solver_type='sharpclaw', test='heterogeneous',
+                                             disable_output=True)
 
-    classichomogeneous_tests   = gen_variants(acoustics_3d_interface.setup, acoustics_verify_homogeneous,
-                                       kernel_languages=('Fortran',), 
-                                       solver_type='classic', test='homogeneous',
-                                       disable_output=True)
-
-    classicheterogeneous_tests = gen_variants(acoustics_3d_interface.setup, acoustics_verify_heterogeneous,
-                                       kernel_languages=('Fortran',), 
-                                       solver_type='classic', test='heterogeneous',
-                                       disable_output=True)
-
-    sharphomogeneous_tests   = gen_variants(acoustics_3d_interface.setup, acoustics_verify_homogeneous,
-                                       kernel_languages=('Fortran',), 
-                                       solver_type='sharpclaw', test='homogeneous',
-                                       disable_output=True)
-
-    sharpheterogeneous_tests = gen_variants(acoustics_3d_interface.setup, acoustics_verify_heterogeneous,
-                                       kernel_languages=('Fortran',), 
-                                       solver_type='sharpclaw', test='heterogeneous',
-                                       disable_output=True)
-    
-    from itertools import chain
-    for test in chain(classichomogeneous_tests, classicheterogeneous_tests, sharphomogeneous_tests, sharpheterogeneous_tests):
+    for test in chain(classic_homogeneous_tests, classic_heterogeneous_tests, sharp_homogeneous_tests,
+                      sharp_heterogeneous_tests):
         yield test
