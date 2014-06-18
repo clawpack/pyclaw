ae81487b65fa6178734130453821f9e2f20864d2
sanromd <damiansra@gmail.com>
2014-06-18 17:29:00 +0300

    minor corrections to dummy tfluct and fixed consistency typos for sharpclaw solver

--- src/pyclaw/sharpclaw/tfluct.f90
+++ src/pyclaw/sharpclaw/tfluct.f90
@@ -1,17 +1,15 @@
-!
-!
 ! =========================================================
-      subroutine tfluct(maxnx,num_eqn,num_waves,num_aux,num_ghost,mx,&
-                        ql,qr,auxl,auxr,amdq2)
+subroutine tfluct(ixyz,maxnx,meqn,mwaves,maux,mbc,mx,ql,qr,auxl,auxr,amdq2)
 ! =========================================================
 !
-!
-      implicit double precision (a-h,o-z)
-!
-!
-      write(*,*) "Error: method(8)=1, but you have not defined"
-      write(*,*) "a function tfluct1()."
-      stop
-!
-      return
-      end
+!	dummy tfluct function (see example euler_1d for more information)
+
+    implicit double precision (a-h,o-z)
+
+    write(*,*) "Error: method(8)=1, but you have not defined"
+    write(*,*) "a function tfluct1()."
+
+    stop
+
+    return
+    end subroutine tfluct
