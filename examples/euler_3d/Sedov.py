13a86e5d9db9aa1464c38e627bbb41cdbc9c4313
Wes Lowrie <wlowrie@uw.edu>
2014-07-14 16:28:32 -0400

    Update Sedov.py
    Made formatting changes to make code more readable.
    Added an argument to switch between split and unsplit methods, so that this example can be used with the rpt3_euler.f90 and rptt3_euler.f90 files that were recently edited.

--- examples/euler_3d/Sedov.py
+++ examples/euler_3d/Sedov.py
@@ -14,40 +14,33 @@
 """
 import numpy as np
 
-#-----------------------------------------------------------------------
 # Constants
-#-----------------------------------------------------------------------
 gamma = 1.4 # Ratio of Specific Heats
 gamma1 = gamma - 1.
 x0 = 0.0; y0 = 0.0; z0 = 0.0 # Sphere location
 rmax = 0.10 # Radius of Sedov Sphere
 
-#-----------------------------------------------------------------------
 # Setup Solver
-#-----------------------------------------------------------------------
-def sedov(kernel_language='Fortran',solver_type='classic',\
-          use_petsc=False,outdir='Sedov_3_output',\
-          output_format='hdf5',file_prefix='sedov',disable_output=False,\
-          mx=256,my=256,mz=256,tfinal=0.10,num_output_times=10):
+def sedov(kernel_language='Fortran', solver_type='classic', use_petsc=False, splitting='unsplit', outdir='Sedov_output', output_format='hdf5', file_prefix='sedov', disable_output=False, mx=256, my=256, mz=256, tfinal=0.10, num_output_times=10):
 
-    #-----------------------------------------------------------------------
     # Load PyClaw
-    #-----------------------------------------------------------------------
     from clawpack import riemann
     if use_petsc:
         import clawpack.petclaw as pyclaw
     else:
         from clawpack import pyclaw
 
-    #-----------------------------------------------------------------------
     # Solver Settings
-    #-----------------------------------------------------------------------
     if solver_type=='classic':
         solver = pyclaw.ClawSolver3D(riemann.euler_3D)
-        solver.dimensional_split = True
+        solver.order = 2
+        if splitting == "split":
+            solver.dimensional_split = True
+        else:
+            solver.dimensional_split = False
+            solver.transverse_waves = 22
         solver.limiters = pyclaw.limiters.tvd.minmod
         solver.num_ghost = 2
-        solver.order = 2
         solver.fwave = False
         solver.num_eqn = 5
         solver.num_waves = 3
@@ -58,37 +51,29 @@ def sedov(kernel_language='Fortran',solver_type='classic',\
     else:
         raise Exception('Unrecognized solver_type.')
 
-    #-----------------------------------------------------------------------
     # Logging
-    #-----------------------------------------------------------------------
     import logging
     solver.logger.setLevel(logging.DEBUG)
 
-    #-----------------------------------------------------------------------
     # Initialize Domain
-    #-----------------------------------------------------------------------
-    x = pyclaw.Dimension('x',-1.0,1.0,mx)
-    y = pyclaw.Dimension('y',-1.0,1.0,my)
-    z = pyclaw.Dimension('z',-1.0,1.0,mz)
+    x = pyclaw.Dimension('x', -1.0, 1.0, mx)
+    y = pyclaw.Dimension('y', -1.0, 1.0, my)
+    z = pyclaw.Dimension('z', -1.0, 1.0, mz)
     domain = pyclaw.Domain([x,y,z])
 
-    #-----------------------------------------------------------------------
     # Define number of waves (eqn) and aux (eps,mu)
-    #-----------------------------------------------------------------------
     num_aux = 0
-    state = pyclaw.State(domain,solver.num_eqn,num_aux)
+    state = pyclaw.State(domain,solver.num_eqn, num_aux)
     state.problem_data['gamma']=gamma
     state.problem_data['gamma1']=gamma1
     
-    #-----------------------------------------------------------------------
     # Initial Conditions
-    #-----------------------------------------------------------------------
     grid = state.grid
     
     x = grid.x.centers
     y = grid.y.centers
     z = grid.z.centers
-    X,Y,Z = np.meshgrid(x,y,z,indexing='ij')
+    X,Y,Z = np.meshgrid(x, y, z, indexing='ij')
     r = np.sqrt((X-x0)**2 + (Y-y0)**2 + (Z-z0)**2)
 
     state.q[0,:,:,:] = 1.0 # density (rho)
@@ -102,21 +87,18 @@ def sedov(kernel_language='Fortran',solver_type='classic',\
     Eblast = 0.851072
     ps = Eblast*gamma1/(4./3.*np.pi*rmax**3)
     p[:,:,:] = eps + ps*(r<=rmax) # pressure
-    vel_sqrd[:,:,:] = (state.q[1,:,:,:]**2+state.q[2,:,:,:]**2+\
-                       state.q[3,:,:,:]**2)/state.q[0,:,:,:]**2
+    vel_sqrd[:,:,:] = (state.q[1,:,:,:]**2 \
+                       + state.q[2,:,:,:]**2 \
+                       + state.q[3,:,:,:]**2)/state.q[0,:,:,:]**2
     state.q[4,:,:,:] = p/gamma1 + 0.5*state.q[0,:,:,:]*vel_sqrd # energy (e)
 
-    #-----------------------------------------------------------------------
     # Setup Boundary Conditions
-    #-----------------------------------------------------------------------
     solver.all_bcs = pyclaw.BC.extrap
 
-    #-----------------------------------------------------------------------
     # Solver Parameters
-    #-----------------------------------------------------------------------
     claw = pyclaw.Controller()
     claw.verbosity = 4
-    claw.solution = pyclaw.Solution(state,domain)
+    claw.solution = pyclaw.Solution(state, domain)
     claw.solver = solver
     claw.output_format = output_format
     claw.output_file_prefix = file_prefix
@@ -129,9 +111,7 @@ def sedov(kernel_language='Fortran',solver_type='classic',\
 
     return claw
 
-#-----------------------------------------------------------------------
 # __main__()
-#-----------------------------------------------------------------------
 if __name__=="__main__":
     from clawpack.pyclaw.util import run_app_from_main
     output = run_app_from_main(sedov)
