a479817955564801c552452d5e0216bc95e533d2
David Ketcheson <dketch@gmail.com>
2014-12-21 13:33:09 +0300

    Modify Travis script to run doctests.
    Also change cleanup.py so it does not run on import, and
    fix a doctest in pyclaw/solver.py.

--- cleanup_examples.py
+++ cleanup_examples.py
@@ -1,32 +1,35 @@
+#!/usr/bin/env python
 
-#
-# Cleans up by deleting object files, executable, and _output directory.
-# Use after run_examples to clean up stuff not needed on webpages.
+r"""
+Cleans up by deleting object files, executable, and _output directory.
+"""
 
 import os,sys,glob
 import shutil
 
-examples_dir = os.path.abspath('./examples')
-print "Will remove all _output, _plots, and build directories from ",examples_dir
-ans = raw_input("Ok? ")
-if ans.lower() not in ['y','yes']:
-    print "Aborting."
-    sys.exit()
+if __name__ == "__main__":
 
-os.chdir(examples_dir)
-exdirlist = []
-for (dirpath, subdirs, files) in os.walk('.'):
-    currentdir = os.path.abspath(os.getcwd())
-    os.chdir(os.path.abspath(dirpath))
-    
-    print 'In directory ',dirpath
-    
-    if os.path.isdir('_output'):
-        shutil.rmtree('./_output')
-    if os.path.isdir('_plots'):
-        shutil.rmtree('./_plots')
-    if os.path.isdir('build'):
-        shutil.rmtree('./build')
+    examples_dir = os.path.abspath('./examples')
+    print "Will remove all _output, _plots, and build directories from ",examples_dir
+    ans = raw_input("Ok? ")
+    if ans.lower() not in ['y','yes']:
+        print "Aborting."
+        sys.exit()
+
+    os.chdir(examples_dir)
+    exdirlist = []
+    for (dirpath, subdirs, files) in os.walk('.'):
+        currentdir = os.path.abspath(os.getcwd())
+        os.chdir(os.path.abspath(dirpath))
         
-    os.chdir(currentdir)
+        print 'In directory ',dirpath
+        
+        if os.path.isdir('_output'):
+            shutil.rmtree('./_output')
+        if os.path.isdir('_plots'):
+            shutil.rmtree('./_plots')
+        if os.path.isdir('build'):
+            shutil.rmtree('./build')
+            
+        os.chdir(currentdir)
 
