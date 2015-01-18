321c8336edcc557c5b5ff949a08917e55d6a883a
Aron Ahmadia <aron@ahmadia.net>
2015-01-18 00:48:04 -0500

    Fast binary-cached build of hashdist dependencies

--- .travis_build_petsc4py.sh
+++ .travis_build_petsc4py.sh
@@ -7,12 +7,16 @@ git clone https://github.com/hashdist/hashdist
     git checkout v0.3
 )
 export PATH=`pwd`/hashdist/bin:$PATH
+hit init-home
 
 # a reasonable profile for building petsc4py on Travis
-cp ../.hashdist_petsc4py_stack.yaml petsc4py_stack.yaml
+mv ../.hashdist_petsc4py_stack.yaml petsc4py_stack.yaml
 
-# This monitors the slow source-based Python/PETSc/petsc4py install
-# Will be much faster with relocatable binary builds when available from <#>
+# 75 MB, shouldn't take too long to download
+wget https://dl.dropboxusercontent.com/u/65439/pyclaw_hashdist_bld.tar.gz
+tar -zxvf pyclaw_hashdist_bld.tar.gz -C ~/.hashdist
+
+# This should now be very fast
 set -o pipefail
 hit develop -v petsc4py_stack.yaml 2>&1 | pv -i 15 -n | cat > log.txt
 tail -n 60 log.txt
