6396998fc4f0e740dc7a5385aab643b3a2cc0c4d
David Ketcheson <dketch@gmail.com>
2015-02-12 12:33:11 +0300

    Fix colormap scale in mapped acoustics example plotting.

--- examples/acoustics_2d_mapped/acoustics_2d_inclusions.py
+++ examples/acoustics_2d_mapped/acoustics_2d_inclusions.py
@@ -272,10 +272,10 @@ def setplot(plotdata):
 
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
     plotitem.plot_var = 1
-    plotitem.pcolor_cmap = colormaps.yellow_red_blue
+    plotitem.pcolor_cmap = 'RdBu'
     plotitem.add_colorbar = True
-    #plotitem.pcolor_cmin = -0.3
-    #plotitem.pcolor_cmax=   0.3
+    plotitem.pcolor_cmin = -1.0
+    plotitem.pcolor_cmax=   1.0
     
     return plotdata
 
