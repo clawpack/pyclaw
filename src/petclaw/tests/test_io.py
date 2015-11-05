d435a6d84f4957c0b8ad35764cd26d53f4cdb41f
Ayman Khalil <aymkhalil85@gmail.com>
2015-11-05 11:30:12 +0300

    Refactor I/O tests as to run parallel ones directly from petclaw

---
+++ src/petclaw/tests/test_io.py
@@ -0,0 +1,23 @@
+from clawpack import pyclaw
+from clawpack import petclaw
+import os
+
+class PetClawIOTest(pyclaw.IOTest):
+    @property
+    def solution(self):
+        return petclaw.Solution()
+
+    @property
+    def file_formats(self):
+        return ['hdf5']
+
+    @property
+    def this_dir(self):
+        return os.path.dirname(os.path.abspath(__file__))
+
+    @property
+    def test_data_dir(self):
+        return os.path.join(self.this_dir, '../../pyclaw/tests/test_data')
+
+    def test_io_from_binary(self):
+        return
