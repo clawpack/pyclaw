deda4061d6120f794a6112f020fca8a818b8668e
Kyle Mandli <kyle.mandli@gmail.com>
2017-03-17 21:08:04 -0400

    Fix some inconsistencies

--- src/pyclaw/io/__init__.py
+++ src/pyclaw/io/__init__.py
@@ -7,24 +7,24 @@ from __future__ import absolute_import
 import logging
 logger = logging.getLogger('pyclaw.io')
 
-from clawpack.pyclaw.io import ascii
-__all__ = ['ascii.read','ascii.write']
+from . import ascii
+__all__ = ['ascii.read', 'ascii.write']
 
-from clawpack.pyclaw.io import binary
+from . import binary
 __all__ += ['binary.read']
 
 # Check for HDF 5 support
 try:
     import h5py
-    import hdf5
+    from . import hdf5
     __all__ += ['hdf5.read', 'hdf5.write']
 except ImportError:
     logger.debug("No hdf5 support found.")
-    
+
 # Check for netcdf support
 try:
     import netCDF4
-    import netcdf
+    from . import netcdf
     __all__ += ['netcdf.read', 'netcdf.write']
 except(ImportError):
     logger.debug("No netcdf4 support found.")
