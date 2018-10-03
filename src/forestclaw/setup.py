f1598d736527c37f99a78c40100cd46c9bf6d15b
David Ketcheson <dketch@gmail.com>
2018-10-03 08:08:47 +0300

    Remove distutils registration of non-existent forestclaw subpackages.

--- src/forestclaw/setup.py
+++ src/forestclaw/setup.py
@@ -5,10 +5,6 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
 
     config = Configuration('forestclaw', parent_package, top_path)
-    config.add_data_files('log.config')
-    config.add_subpackage('classic')
-    config.add_subpackage('sharpclaw')
-    config.add_subpackage('limiters')
     config.add_subpackage('fileio')
     return config
 
