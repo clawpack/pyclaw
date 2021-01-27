clawpack/pyclaw

commit b9706ae73e903ac27ee69c9c71c092852b36695c

Improve formatting of f90 code in classic.

src/pyclaw/classic/philim.f90
@@ -1,58 +1,37 @@
-
-
-!     =====================================================
-    double precision function philim(a,b,meth)
-!     =====================================================
+! =====================================================
+double precision function philim(a,b,meth)
+! =====================================================
     implicit double precision (a-h,o-z)
 
-!     # Compute a limiter based on wave strengths a and b.
-!     # meth determines what limiter is used.
-!     # a is assumed to be nonzero.
+! Compute a limiter based on wave strengths a and b.
+! meth determines what limiter is used.
+! a is assumed to be nonzero.
 
-!     # NOTE: This routine is obsolete.  Instead of using limiter.f,
-!     # which calls philim.f for every wave, it is more efficient to
-!     # use inlinelimiter.f, which eliminates all these function calls
-!     # to philim.  If you wish to change the limiter function and are
-!     # using inlinelimiter.f, the formulas must be changed in that routine.
+! NOTE: This routine is obsolete.  Instead of using limiter.f,
+! which calls philim.f for every wave, it is more efficient to
+! use inlinelimiter.f, which eliminates all these function calls
+! to philim.  If you wish to change the limiter function and are
+! using inlinelimiter.f, the formulas must be changed in that routine.
 
     r = b/a
-    go to (10,20,30,40,50) meth
+    select case (meth)
 
+    case (10) ! minmod
+        philim = dmax1(0.d0, dmin1(1.d0, r))
 
-    10 continue
-!     --------
-!     # minmod
-!     --------
-    philim = dmax1(0.d0, dmin1(1.d0, r))
+    case (20) ! superbee
+        philim = dmax1(0.d0, dmin1(1.d0, 2.d0*r), dmin1(2.d0, r))
+
+    case (30) ! van Leer
+        philim = (r + dabs(r)) / (1.d0 + dabs(r))
+
+    case (40) ! monotonized centered
+        c = (1.d0 + r)/2.d0
+        philim = dmax1(0.d0, dmin1(c, 2.d0, 2.d0*r))
+
+    case (50) ! Beam-Warming
+        philim = r
+
+    end select
     return
-
-    20 continue
-!     ----------
-!     # superbee
-!     ----------
-    philim = dmax1(0.d0, dmin1(1.d0, 2.d0*r), dmin1(2.d0, r))
-    return
-
-    30 continue
-!     ----------
-!     # van Leer
-!     ----------
-    philim = (r + dabs(r)) / (1.d0 + dabs(r))
-    return
-
-    40 continue
-!     ------------------------------
-!     # monotinized centered
-!     ------------------------------
-    c = (1.d0 + r)/2.d0
-    philim = dmax1(0.d0, dmin1(c, 2.d0, 2.d0*r))
-    return
-
-    50 continue
-!     ------------------------------
-!     # Beam-Warming
-!     ------------------------------
-    philim = r
-
-    return
-    END FUNCTION
+end function

src/pyclaw/classic/flux2.f90
@@ -1,38 +1,38 @@
-!     =====================================================
-    subroutine flux2(ixy,maxm,num_eqn,num_waves,num_aux,num_ghost,mx, &
-    q1d,dtdx1d,aux1,aux2,aux3,method,mthlim, &
-    qadd,fadd,gadd,cfl1d,wave,s, &
-    amdq,apdq,cqxx,bmasdq,bpasdq,rpn2,rpt2,use_fwave)
-!     =====================================================
+! =====================================================
+subroutine flux2(ixy,maxm,num_eqn,num_waves,num_aux,num_ghost,mx, &
+                 q1d,dtdx1d,aux1,aux2,aux3,method,mthlim, &
+                 qadd,fadd,gadd,cfl1d,wave,s, &
+                 amdq,apdq,cqxx,bmasdq,bpasdq,rpn2,rpt2,use_fwave)
+! =====================================================
 
-!     # Compute the modification to fluxes f and g that are generated by
-!     # all interfaces along a 1D slice of the 2D patch.
-!     #    ixy = 1  if it is a slice in x
-!     #          2  if it is a slice in y
-!     # This value is passed into the Riemann solvers. The flux modifications
-!     # go into the arrays fadd and gadd.  The notation is written assuming
-!     # we are solving along a 1D slice in the x-direction.
+! Compute the modification to fluxes f and g that are generated by
+! all interfaces along a 1D slice of the 2D patch.
+! ixy = 1  if it is a slice in x
+! 2  if it is a slice in y
+! This value is passed into the Riemann solvers. The flux modifications
+! go into the arrays fadd and gadd.  The notation is written assuming
+! we are solving along a 1D slice in the x-direction.
 
-!     # fadd(i,.) modifies F to the left of cell i
-!     # gadd(i,.,1) modifies G below cell i
-!     # gadd(i,.,2) modifies G above cell i
+! fadd(i,.) modifies F to the left of cell i
+! gadd(i,.,1) modifies G below cell i
+! gadd(i,.,2) modifies G above cell i
 
-!     # The method used is specified by method(2:3):
+! The method used is specified by method(2:3):
 
-!         method(2) = 1 if only first order increment waves are to be used.
-!                   = 2 if second order correction terms are to be added, with
+! method(2) = 1 if only first order increment waves are to be used.
+!           = 2 if second order correction terms are to be added, with
 !                       a flux limiter as specified by mthlim.
 
-!         method(3) = 0 if no transverse propagation is to be applied.
+! method(3) = 0 if no transverse propagation is to be applied.
 !                       Increment and perhaps correction waves are propagated
 !                       normal to the interface.
-!                   = 1 if transverse propagation of increment waves
+!           = 1 if transverse propagation of increment waves
 !                       (but not correction waves, if any) is to be applied.
-!                   = 2 if transverse propagation of correction waves is also
+!           = 2 if transverse propagation of correction waves is also
 !                       to be included.
 
-!     Note that if method(6)=1 then the capa array comes into the second
-!     order correction terms, and is already included in dtdx1d:
+! Note that if method(6)=1 then the capa array comes into the second
+! order correction terms, and is already included in dtdx1d:
 !     If ixy = 1 then
 !        dtdx1d(i) = dt/dx                 if method(6) = 0
 !                  = dt/(dx*capa(i,jcom))  if method(6) = 1
@@ -40,7 +40,7 @@
 !        dtdx1d(j) = dt/dy                 if method(6) = 0
 !                  = dt/(dy*capa(icom,j))  if method(6) = 1
 
-!     Notation:
+! Notation:
 !        The jump in q (q1d(i,:)-q1d(i-1,:))  is split by rpn2 into
 !            amdq =  the left-going flux difference  A^- Delta q
 !            apdq = the right-going flux difference  A^+ Delta q
@@ -80,60 +80,60 @@
     common /comxyt/ dtcom,dxcom,dycom,tcom,icom,jcom
 
     limit = .false.
-    do 5 mw=1,num_waves
+    do mw=1,num_waves
         if (mthlim(mw) > 0) limit = .TRUE. 
-    5 END DO
+    end do
 
-!     # initialize flux increments:
-!     -----------------------------
+    ! initialize flux increments:
+    ! -----------------------------
 
     forall (m=1:num_eqn, i = 1-num_ghost: mx+num_ghost)
-    qadd(m,i) = 0.d0
-    fadd(m,i) = 0.d0
-    gadd(m,1,i) = 0.d0
-    gadd(m,2,i) = 0.d0
+        qadd(m,i) = 0.d0
+        fadd(m,i) = 0.d0
+        gadd(m,1,i) = 0.d0
+        gadd(m,2,i) = 0.d0
     end forall
       
 
-!     # solve Riemann problem at each interface and compute Godunov updates
-!     ---------------------------------------------------------------------
+    ! solve Riemann problem at each interface and compute Godunov updates
+    ! ---------------------------------------------------------------------
 
     call rpn2(ixy,maxm,num_eqn,num_waves,num_aux,num_ghost,mx,q1d,q1d, &
-    aux2,aux2,wave,s,amdq,apdq)
+              aux2,aux2,wave,s,amdq,apdq)
 
-!     # Set qadd for the donor-cell upwind method (Godunov)
+    ! Set qadd for the donor-cell upwind method (Godunov)
     forall(m=1:num_eqn, i=1:mx+1)
-    qadd(m,i) = qadd(m,i) - dtdx1d(i)*apdq(m,i)
-    qadd(m,i-1) = qadd(m,i-1) - dtdx1d(i-1)*amdq(m,i)
+        qadd(m,i) = qadd(m,i) - dtdx1d(i)*apdq(m,i)
+        qadd(m,i-1) = qadd(m,i-1) - dtdx1d(i-1)*amdq(m,i)
     end forall
 
-!     # compute maximum wave speed for checking Courant number:
+    ! compute maximum wave speed for checking Courant number:
     cfl1d = 0.d0
-    do 51 mw=1,num_waves
-        do 50 i=1,mx+1
-        !          # if s>0 use dtdx1d(i) to compute CFL,
-        !          # if s<0 use dtdx1d(i-1) to compute CFL:
+    do mw=1,num_waves
+        do i=1,mx+1
+            ! if s>0 use dtdx1d(i) to compute CFL,
+            ! if s<0 use dtdx1d(i-1) to compute CFL:
             cfl1d = dmax1(cfl1d, dtdx1d(i)*s(mw,i), &
             -dtdx1d(i-1)*s(mw,i))
-        50 END DO
-    51 END DO
+        end do
+    end do
 
     if (method(2) == 1) go to 130
 
-!     # modify F fluxes for second order q_{xx} correction terms:
-!     -----------------------------------------------------------
+    ! modify F fluxes for second order q_{xx} correction terms:
+    ! -----------------------------------------------------------
 
-!     # apply limiter to waves:
+    ! apply limiter to waves:
     if (limit) call limiter(maxm,num_eqn,num_waves,num_ghost,mx, &
-    wave,s,mthlim)
+                            wave,s,mthlim)
 
-!     # For correction terms below, need average of dtdx in cell
-!     # i-1 and i.  We compute these and overwrite dtdx1d.
+    ! For correction terms below, need average of dtdx in cell
+    ! i-1 and i.  We compute these and overwrite dtdx1d.
 
-!     # Second order corrections:
+    ! Second order corrections:
     if (use_fwave.eqv. .FALSE. ) then
         do i = 2-num_ghost, mx+num_ghost
-        !            # modified in Version 4.3 to use average only in cqxx, not transverse
+            ! modified in Version 4.3 to use average only in cqxx, not transverse
             dtdxave = 0.5d0 * (dtdx1d(i-1) + dtdx1d(i))
             do m=1,num_eqn
                 cqxx(m,i) = 0.d0
@@ -165,43 +165,43 @@
     if (method(3) <= 0) go to 999   !# no transverse propagation
 
     if (method(2) > 1 .AND. method(3) == 2) then
-    !        # incorporate cqxx into amdq and apdq so that it is split also.
+        ! incorporate cqxx into amdq and apdq so that it is split also.
         forall (m=1:num_eqn, i = 1: mx+1)
-        amdq(m,i) = amdq(m,i) + cqxx(m,i)
-        apdq(m,i) = apdq(m,i) - cqxx(m,i)
+            amdq(m,i) = amdq(m,i) + cqxx(m,i)
+            apdq(m,i) = apdq(m,i) - cqxx(m,i)
         end forall
     endif
 
 
-!      # modify G fluxes for transverse propagation
-!      --------------------------------------------
+    ! modify G fluxes for transverse propagation
+    ! --------------------------------------------
 
 
-!     # split the left-going flux difference into down-going and up-going:
+    ! split the left-going flux difference into down-going and up-going:
     call rpt2(ixy,1,maxm,num_eqn,num_waves,num_aux,num_ghost,mx,q1d,q1d, &
-    aux1,aux2,aux3,amdq,bmasdq,bpasdq)
+              aux1,aux2,aux3,amdq,bmasdq,bpasdq)
 
-!     # modify flux below and above by B^- A^- Delta q and  B^+ A^- Delta q:
+    ! modify flux below and above by B^- A^- Delta q and  B^+ A^- Delta q:
     forall ( m=1:num_eqn , i = 1:mx+1)
-    gadd(m,1,i-1) = gadd(m,1,i-1) - &
-    0.5d0*dtdx1d(i-1) * bmasdq(m,i)
-    gadd(m,2,i-1) = gadd(m,2,i-1) - &
-    0.5d0*dtdx1d(i-1) * bpasdq(m,i)
+        gadd(m,1,i-1) = gadd(m,1,i-1) - &
+        0.5d0*dtdx1d(i-1) * bmasdq(m,i)
+        gadd(m,2,i-1) = gadd(m,2,i-1) - &
+        0.5d0*dtdx1d(i-1) * bpasdq(m,i)
     end forall
      
 
-!     # split the right-going flux difference into down-going and up-going:
+    ! split the right-going flux difference into down-going and up-going:
     call rpt2(ixy,2,maxm,num_eqn,num_waves,num_aux,num_ghost,mx,q1d,q1d, &
-    aux1,aux2,aux3,apdq,bmasdq,bpasdq)
+              aux1,aux2,aux3,apdq,bmasdq,bpasdq)
 
-!     # modify flux below and above by B^- A^+ Delta q and  B^+ A^+ Delta q:
+    ! modify flux below and above by B^- A^+ Delta q and  B^+ A^+ Delta q:
     forall ( m=1:num_eqn , i = 1: mx+1)
-    gadd(m,1,i) = gadd(m,1,i) - &
-    0.5d0*dtdx1d(i) * bmasdq(m,i)
-    gadd(m,2,i) = gadd(m,2,i) - &
-    0.5d0*dtdx1d(i) * bpasdq(m,i)
+        gadd(m,1,i) = gadd(m,1,i) - &
+        0.5d0*dtdx1d(i) * bmasdq(m,i)
+        gadd(m,2,i) = gadd(m,2,i) - &
+        0.5d0*dtdx1d(i) * bpasdq(m,i)
     end forall
 
     999 continue
     return
-    end subroutine flux2
+end subroutine flux2

src/pyclaw/classic/limiter.f90
@@ -1,49 +1,45 @@
+! =====================================================
+subroutine limiter(maxm,num_eqn,num_waves,num_ghost,mx,wave,s,mthlim)
+! =====================================================
 
+! Apply a limiter to the waves.
+! The limiter is computed by comparing the 2-norm of each wave with
+! the projection of the wave from the interface to the left or
+! right onto the current wave.  For a linear system this would
+! correspond to comparing the norms of the two waves.  For a
+! nonlinear problem the eigenvectors are not colinear and so the
+! projection is needed to provide more limiting in the case where the
+! neighboring wave has large norm but points in a different direction
+! in phase space.
 
-!     =====================================================
-    subroutine limiter(maxm,num_eqn,num_waves,num_ghost,mx,wave,s, &
-    mthlim)
-!     =====================================================
+! The specific limiter used in each family is determined by the
+! value of the corresponding element of the array mthlim, as used in
+! the function philim.
+! Note that a different limiter may be used in each wave family.
 
-!     # Apply a limiter to the waves.
-!     # The limiter is computed by comparing the 2-norm of each wave with
-!     # the projection of the wave from the interface to the left or
-!     # right onto the current wave.  For a linear system this would
-!     # correspond to comparing the norms of the two waves.  For a
-!     # nonlinear problem the eigenvectors are not colinear and so the
-!     # projection is needed to provide more limiting in the case where the
-!     # neighboring wave has large norm but points in a different direction
-!     # in phase space.
-
-!     # The specific limiter used in each family is determined by the
-!     # value of the corresponding element of the array mthlim, as used in
-!     # the function philim.
-!     # Note that a different limiter may be used in each wave family.
-
-!     # dotl and dotr denote the inner product of wave with the wave to
-!     # the left or right.  The norm of the projections onto the wave are then
-!     # given by dotl/wnorm2 and dotr/wnorm2, where wnorm2 is the 2-norm
-!     # of wave.
+! dotl and dotr denote the inner product of wave with the wave to
+! the left or right.  The norm of the projections onto the wave are then
+! given by dotl/wnorm2 and dotr/wnorm2, where wnorm2 is the 2-norm
+! of wave.
 
     implicit double precision (a-h,o-z)
     dimension mthlim(num_waves)
     dimension wave(num_eqn, num_waves,1-num_ghost:maxm+num_ghost)
     dimension    s(num_waves,1-num_ghost:maxm+num_ghost)
 
-
-    do 50 mw=1,num_waves
-        if (mthlim(mw) == 0) go to 50
+    do mw=1,num_waves
+        if (mthlim(mw) == 0) cycle
         dotr = 0.d0
-        do 40 i = 0, mx+1
+        do i = 0, mx+1
             wnorm2 = 0.d0
             dotl = dotr
             dotr = 0.d0
-            do 20 m=1,num_eqn
+            do m=1,num_eqn
                 wnorm2 = wnorm2 + wave(m,mw,i)**2
                 dotr = dotr + wave(m,mw,i)*wave(m,mw,i+1)
-            20 END DO
-            if (i == 0) go to 40
-            if (wnorm2 == 0.d0) go to 40
+            end do
+            if (i == 0) cycle
+            if (wnorm2 == 0.d0) cycle
         
             if (s(mw,i) > 0.d0) then
                 wlimitr = philim(wnorm2, dotl, mthlim(mw))
@@ -51,11 +47,11 @@
                 wlimitr = philim(wnorm2, dotr, mthlim(mw))
             endif
         
-            do 30 m=1,num_eqn
+            do m=1,num_eqn
                 wave(m,mw,i) = wlimitr * wave(m,mw,i)
-            30 END DO
-        40 END DO
-    50 END DO
+            end do
+        end do
+    end do
 
     return
-    end subroutine limiter
+end subroutine limiter

src/pyclaw/classic/step1.f90
@@ -1,37 +1,32 @@
-
-
 ! ===================================================================
-    subroutine step1(num_eqn,num_waves,num_ghost,num_aux,mx,q,aux,dx, &
-    dt,method,mthlim,cfl,f,wave,s,amdq,apdq,dtdx,use_fwave,rp1)
+subroutine step1(num_eqn,num_waves,num_ghost,num_aux,mx,q,aux,dx,dt, &
+                method,mthlim,cfl,f,wave,s,amdq,apdq,dtdx,use_fwave,rp1)
 ! ===================================================================
 
-!     # Take one time step, updating q.
+! Take one time step, updating q.
 
-!     method(1) = 1   ==>  Godunov method
-!     method(1) = 2   ==>  Slope limiter method
-!     mthlim(p)  controls what limiter is used in the pth family
+! method(1) = 1   ==>  Godunov method
+! method(1) = 2   ==>  Slope limiter method
+! mthlim(p)  controls what limiter is used in the pth family
 
+! amdq, apdq, wave, s, and f are used locally:
 
-!     amdq, apdq, wave, s, and f are used locally:
-
-!     amdq(1-num_ghost:mx+num_ghost, num_eqn) = left-going flux-differences
-!     apdq(1-num_ghost:mx+num_ghost, num_eqn) = right-going flux-differences
+! amdq(1-num_ghost:mx+num_ghost, num_eqn) = left-going flux-differences
+! apdq(1-num_ghost:mx+num_ghost, num_eqn) = right-going flux-differences
 !        e.g. amdq(m,i) = m'th component of A^- \Delta q from i'th Riemann
 !                         problem (between cells i-1 and i).
 
-!     wave(1-num_ghost:mx+num_ghost, num_eqn, num_waves) = waves from solution of
+! wave(1-num_ghost:mx+num_ghost, num_eqn, num_waves) = waves from solution of
 !                                           Riemann problems,
-!            wave(m,mw,i) = mth component of jump in q across
-!                           wave in family mw in Riemann problem between
-!                           states i-1 and i.
+! wave(m,mw,i) = mth component of jump in q across wave in family mw in Riemann
+!                   problem between states i-1 and i.
 
-!     s(1-num_ghost:mx+num_ghost, num_waves) = wave speeds,
-!            s(m,iw) = speed of wave in family mw in Riemann problem between
-!                      states i-1 and i.
+! s(1-num_ghost:mx+num_ghost, num_waves) = wave speeds,
+! s(m,iw) = speed of wave in family mw in Riemann problem between states i-1 and i.
 
-!     f(1-num_ghost:mx+num_ghost, num_eqn) = correction fluxes for second order method
-!            f(m,i) = mth component of flux at left edge of ith cell
-!     --------------------------------------------------------------------
+! f(1-num_ghost:mx+num_ghost, num_eqn) = correction fluxes for second order method
+! f(m,i) = mth component of flux at left edge of ith cell
+! --------------------------------------------------------------------
 
     implicit double precision (a-h,o-z)
     double precision :: q(num_eqn,1-num_ghost:mx+num_ghost)
@@ -55,14 +50,14 @@
 !f2py optional f, amdq, apdq, dtdx, s, wave
 
 
-!     # check if any limiters are used:
+    ! check if any limiters are used:
     limit = .false.
-    do 5 mw=1,num_waves
+    do mw=1,num_waves
         if (mthlim(mw) > 0) limit = .TRUE. 
-    5 END DO
+    end do
 
     index_capa = method(6)
-    do 10 i=1-num_ghost,mx+num_ghost
+    do i=1-num_ghost,mx+num_ghost
         if (index_capa > 0) then
             if (aux(index_capa,i) <= 0.d0) then
                 write(6,*) 'Error -- capa must be positive'
@@ -72,87 +67,82 @@
         else
             dtdx(i) = dt/dx
         endif
-    10 END DO
+    end do
 
 
 
-!     # solve Riemann problem at each interface
-!     -----------------------------------------
+    ! solve Riemann problem at each interface
+    ! -----------------------------------------
 
     call rp1(mx,num_eqn,num_waves,num_aux,num_ghost,mx,q,q,aux,aux,wave, &
     s,amdq,apdq)
 
-!     # Modify q for Godunov update:
-!     # Note this may not correspond to a conservative flux-differencing
-!     # for equations not in conservation form.  It is conservative if
-!     # amdq + apdq = f(q(i)) - f(q(i-1)).
-
-
-
-
-          
-!     print *,"q before, from fortran",q(24,1)
+    ! Modify q for Godunov update:
+    ! Note this may not correspond to a conservative flux-differencing
+    ! for equations not in conservation form.  It is conservative if
+    ! amdq + apdq = f(q(i)) - f(q(i-1)).
 
     forall(i=1:mx+1, m=1:num_eqn)
-    q(m,i) = q(m,i) - dtdx(i)*apdq(m,i)
-    q(m,i-1) = q(m,i-1) - dtdx(i-1)*amdq(m,i)
+        q(m,i) = q(m,i) - dtdx(i)*apdq(m,i)
+        q(m,i-1) = q(m,i-1) - dtdx(i-1)*amdq(m,i)
     end forall
 
 
-!     # compute maximum wave speed:
+    ! compute maximum wave speed:
     cfl = 0.d0
-    do 50 mw=1,num_waves
-        do 45 i=1,mx+1
-        !          # if s>0 use dtdx(i) to compute CFL,
-        !          # if s<0 use dtdx(i-1) to compute CFL:
+    do mw=1,num_waves
+        do i=1,mx+1
+        ! if s>0 use dtdx(i) to compute CFL,
+        ! if s<0 use dtdx(i-1) to compute CFL:
             cfl = dmax1(cfl, dtdx(i)*s(mw,i), -dtdx(i-1)*s(mw,i))
-        45 END DO
-    50 END DO
+        end do
+    end do
 
     if (method(2) == 1) go to 900
 
-!     # compute correction fluxes for second order q_{xx} terms:
-!     ----------------------------------------------------------
+    ! compute correction fluxes for second order q_{xx} terms:
+    ! ----------------------------------------------------------
 
     forall(i=1-num_ghost:mx+num_ghost, m=1:num_eqn)
-    f(m,i) = 0.d0
+        f(m,i) = 0.d0
     end forall
 
-!      # apply limiter to waves:
+    ! apply limiter to waves:
     if (limit) call limiter(mx,num_eqn,num_waves,num_ghost,mx, &
     wave,s,mthlim)
 
     if (use_fwave.eqv. .FALSE. ) then
-        do 120 i=1,mx+1
-            do 120 m=1,num_eqn
-                do 110 mw=1,num_waves
+        do i=1,mx+1
+            do m=1,num_eqn
+                do mw=1,num_waves
                     dtdxave = 0.5d0 * (dtdx(i-1) + dtdx(i))
                     f(m,i) = f(m,i) + 0.5d0 * dabs(s(mw,i)) &
                     * (1.d0 - dabs(s(mw,i))*dtdxave) * wave(m,mw,i)
-                110 END DO
-        120 END DO
+                end do
+            end do
+        end do
     else
-        do 121 i=1,mx+1
-            do 121 m=1,num_eqn
-                do 111 mw=1,num_waves
+        do i=1,mx+1
+            do m=1,num_eqn
+                do mw=1,num_waves
                     dtdxave = 0.5d0 * (dtdx(i-1) + dtdx(i))
                     f(m,i) = f(m,i) + 0.5d0 * dsign(1.d0,s(mw,i)) &
                     * (1.d0 - dabs(s(mw,i))*dtdxave) * wave(m,mw,i)
-                111 END DO
-        121 END DO
+                end do
+            end do
+        end do
     endif
 
 
+    ! update q by differencing correction fluxes
+    ! ============================================
 
-!     # update q by differencing correction fluxes
-!     ============================================
-
-!     # (Note:  Godunov update has already been performed above)
+    ! (Note:  Godunov update has already been performed above)
 
     forall(i=1:mx+1, m=1:num_eqn)
-    q(m,i) = q(m,i) - dtdx(i) * (f(m,i+1) - f(m,i))
+        q(m,i) = q(m,i) - dtdx(i) * (f(m,i+1) - f(m,i))
     end forall
 
     900 continue
     return
-    end subroutine step1
+end subroutine step1

commit f90b0881998055ef32585a7ff303044ff4f08147

Correct labels in philim.f90 select statement.
This fixes a bug introduced in b9706ae73e903ac27ee69c9c71c092852b36695c.

src/pyclaw/classic/philim.f90
@@ -16,20 +16,20 @@ double precision function philim(a,b,meth)
     r = b/a
     select case (meth)
 
-    case (10) ! minmod
+    case (1) ! minmod
         philim = dmax1(0.d0, dmin1(1.d0, r))
 
-    case (20) ! superbee
+    case (2) ! superbee
         philim = dmax1(0.d0, dmin1(1.d0, 2.d0*r), dmin1(2.d0, r))
 
-    case (30) ! van Leer
+    case (3) ! van Leer
         philim = (r + dabs(r)) / (1.d0 + dabs(r))
 
-    case (40) ! monotonized centered
+    case (4) ! monotonized centered
         c = (1.d0 + r)/2.d0
         philim = dmax1(0.d0, dmin1(c, 2.d0, 2.d0*r))
 
-    case (50) ! Beam-Warming
+    case (5) ! Beam-Warming
         philim = r
 
     end select